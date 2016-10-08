01a74720320ed317cc2877b57e97f334db4a7756
Teodor Moiseev <femoiseev@gmail.com>
2016-10-08 19:30:54 +0300

    reformat code

--- homework-g597-moiseev/src/main/java/ru/mipt/java2016/homework/g597/moiseev/task1/StackCalculator.java
+++ homework-g597-moiseev/src/main/java/ru/mipt/java2016/homework/g597/moiseev/task1/StackCalculator.java
@@ -1,8 +1,11 @@
 package ru.mipt.java2016.homework.g597.moiseev.task1;
 
-import ru.mipt.java2016.homework.base.task1.*;
+import ru.mipt.java2016.homework.base.task1.Calculator;
+import ru.mipt.java2016.homework.base.task1.ParsingException;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * Стековый калькулятор.
@@ -15,7 +18,7 @@ public class StackCalculator implements Calculator {
 
     @Override
     public double calculate(String expression) throws ParsingException {
-        if(expression == null) {
+        if (expression == null) {
             throw new ParsingException("Expression is null");
         }
         String postfix_line = getPostfixLine(expression.replaceAll("\\s", "")); // Преобразуем инфикссную запись в постфиксную
@@ -26,19 +29,18 @@ public class StackCalculator implements Calculator {
         boolean flag = true; // Флажок на то, что следующий оператор - унарный
         Stack<Character> stack = new Stack<>(); // Стек операторов
         String result = ""; // Результирующая строка
-        for(Character c : expression.toCharArray()) { // Перебираем элементы строки
-            if(c.equals(' ')) {}
-            else if(Arrays.asList(numbers_and_dot).contains(c)) { // Если символ - элемент числа
+        for (Character c : expression.toCharArray()) { // Перебираем элементы строки
+            if (c.equals(' ')) {
+            } else if (Arrays.asList(numbers_and_dot).contains(c)) { // Если символ - элемент числа
                 flag = false;
                 result += c.toString(); // то добавляем его к результату
-            } else if(Arrays.asList(operators).contains(c)) { // Если оператор
-                if(flag) { // Если он унарный
-                    if(c.equals('+')) {
+            } else if (Arrays.asList(operators).contains(c)) { // Если оператор
+                if (flag) { // Если он унарный
+                    if (c.equals('+')) {
                         flag = false;
                     } else if (c.equals('-')) { // То дописываем его к результату
                         result += "-";
-                    }
-                    else {
+                    } else {
                         throw new ParsingException("Invalid expression");
                     }
                 } else { // Иначе
@@ -55,11 +57,11 @@ public class StackCalculator implements Calculator {
                     }
                     stack.push(c); // Помещаем оператор в стек
                 }
-            } else if(c.equals('(')) { // Если открывающая скобка
+            } else if (c.equals('(')) { // Если открывающая скобка
                 flag = true;
                 result += " ";
                 stack.push(c); // То помещаем ее в стек
-            } else if(c.equals(')')) { // Если закрывающая скобка
+            } else if (c.equals(')')) { // Если закрывающая скобка
                 flag = false;
                 boolean is_opening_bracket = false;
                 while (!stack.empty()) { // То выталкиваем элементы из стека
@@ -79,7 +81,7 @@ public class StackCalculator implements Calculator {
             }
         }
 
-        while(!stack.empty()) { // Выталкиваем оставшиеся элементы из стека
+        while (!stack.empty()) { // Выталкиваем оставшиеся элементы из стека
             Character current = stack.pop();
             if (Arrays.asList(operators).contains(current)) {
                 result += (" " + current.toString() + " ");
@@ -93,13 +95,26 @@ public class StackCalculator implements Calculator {
     protected int getPriority(char c) throws ParsingException { // Приоритет оператора
         int priority;
         switch (c) {
-            case '+': priority = 1; break;
-            case '-': priority = 1; break;
-            case '*': priority = 2; break;
-            case '/': priority = 2; break;
-            case '(': priority = 0; break;
-            case ')': priority = 0; break;
-            default: throw new ParsingException("Invalid symbol");
+            case '+':
+                priority = 1;
+                break;
+            case '-':
+                priority = 1;
+                break;
+            case '*':
+                priority = 2;
+                break;
+            case '/':
+                priority = 2;
+                break;
+            case '(':
+                priority = 0;
+                break;
+            case ')':
+                priority = 0;
+                break;
+            default:
+                throw new ParsingException("Invalid symbol");
         }
         return priority;
     }
@@ -112,11 +127,14 @@ public class StackCalculator implements Calculator {
                 res = v1 + v2;
                 break;
             case '-':
-                res =  v1 - v2; break;
+                res = v1 - v2;
+                break;
             case '*':
-                res = v1 * v2; break;
+                res = v1 * v2;
+                break;
             case '/':
-                res = v1 / v2; break;
+                res = v1 / v2;
+                break;
             default:
                 throw new ParsingException("Invalid symbol");
         }
@@ -128,8 +146,8 @@ public class StackCalculator implements Calculator {
         Stack<Double> stack = new Stack<>(); // Стек промежуточных результатов
         while (sc.hasNext()) { // Перебираем все лексемы в выражении
             String s = sc.next();
-            if(s.length() == 1 && Arrays.asList(operators).contains(s.charAt(0))) { // Если это оператор
-                if(stack.size() >= 2) { // То применяем его к двум верхним элементам стека
+            if (s.length() == 1 && Arrays.asList(operators).contains(s.charAt(0))) { // Если это оператор
+                if (stack.size() >= 2) { // То применяем его к двум верхним элементам стека
                     double operand2 = stack.pop();
                     double operand1 = stack.pop();
                     double result = calculateSingleOperation(operand1, operand2, s.charAt(0));
@@ -143,7 +161,7 @@ public class StackCalculator implements Calculator {
             }
         }
 
-        if(stack.size() == 1) { // В коонце в стеке должен остаться один элемент
+        if (stack.size() == 1) { // В коонце в стеке должен остаться один элемент
             return stack.pop(); // И это результат
         } else {
             throw new ParsingException("Invalid expression");
