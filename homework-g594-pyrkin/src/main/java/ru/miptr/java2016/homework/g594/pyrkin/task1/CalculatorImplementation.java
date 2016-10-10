3ae4912083185555e7ac9ecfb5598d028cc1c2e4
randan <alagaster@yandex.ru>
2016-10-10 06:36:13 +0300

    All tests work.

--- homework-g594-pyrkin/src/main/java/ru/miptr/java2016/homework/g594/pyrkin/task1/CalculatorImplementation.java
+++ homework-g594-pyrkin/src/main/java/ru/miptr/java2016/homework/g594/pyrkin/task1/CalculatorImplementation.java
@@ -1,27 +1,27 @@
 package ru.miptr.java2016.homework.g594.pyrkin.task1;
 
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
-import com.sun.org.apache.xerces.internal.impl.xs.identity.Selector;
-import com.sun.org.apache.xml.internal.dtm.ref.dom2dtm.DOM2DTM;
-import com.sun.org.apache.xml.internal.serializer.utils.SystemIDResolver;
 import ru.mipt.java2016.homework.base.task1.Calculator;
 import ru.mipt.java2016.homework.base.task1.ParsingException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import java.util.regex.Pattern;
+import java.util.Stack;
 
 /**
  * 2-stack Calculator
  * Created by randan on 10/9/16.
  */
 public class CalculatorImplementation implements Calculator{
+    private Stack<Double> numbers = new Stack<>();
+    private Stack<Character> symbols = new Stack<>();
+    private double currentInteger;
+    private double currentDecimal;
+    private boolean point;
+    private boolean number;
+
     private boolean badSymbolsCheck(String expression){
         for(char symbol : expression.toCharArray())
             if(!Character.isDigit(symbol) && symbol != '.' && symbol != '(' && symbol != ')'
                 && symbol != '+' && symbol != '-' && symbol != '*' && symbol !='/')
-                return false;
-        return true;
+                return true;
+        return false;
     }
 
     private boolean bracketsCheck(String expression){
@@ -33,93 +33,171 @@ public class CalculatorImplementation implements Calculator{
             else if(symbol == ')') {
                 --balance;
                 if(previousSymbol == '(')
-                    return false;
+                    return true;
             }
 
             if(balance < 0)
-                return false;
+                return true;
 
             previousSymbol = symbol;
         }
-        return balance == 0;
+        return balance != 0;
     }
 
-    private String removeExtraOperands(String expression){
-        Pattern pattern = Pattern.compile("([\\d)])\\+*-(\\+*-\\+*-)*\\+*([\\d(])");
-        Matcher matcher = pattern.matcher(expression);
-        expression = matcher.replaceAll("$1-$3");
-
-        pattern = Pattern.compile("([\\d)])(\\+*-\\+*-)*\\+*--(\\+*-\\+*-)*\\+*([\\d(])");
-        matcher = pattern.matcher(expression);
-        expression = matcher.replaceAll("$1+$4");
-
-        pattern = Pattern.compile("([\\d)])(\\+*-\\+*-)*\\+(\\+*-\\+*-)*\\+*([\\d(])");
-        matcher = pattern.matcher(expression);
-        expression = matcher.replaceAll("$1+$4");
-
-        pattern = Pattern.compile("\\(\\+*-(\\+*-\\+*-)*\\+*([\\d(])");
-        matcher = pattern.matcher(expression);
-        expression = matcher.replaceAll("(-$2");
-
-        pattern = Pattern.compile("\\((\\+*-\\+*-)*\\+*--(\\+*-\\+*-)*\\+*([\\d(])");
-        matcher = pattern.matcher(expression);
-        expression = matcher.replaceAll("($3");
-
-        pattern = Pattern.compile("\\((\\+*-\\+*-)*\\+(\\+*-\\+*-)*\\+*([\\d(])");
-        matcher = pattern.matcher(expression);
-        expression = matcher.replaceAll("($3");
-
-        pattern = Pattern.compile("^\\+*-(\\+*-\\+*-)*\\+*([\\d(])");
-        matcher = pattern.matcher(expression);
-        expression = matcher.replaceAll("-$2");
-
-        pattern = Pattern.compile("^(\\+*-\\+*-)*\\+*--(\\+*-\\+*-)*\\+*([\\d(])");
-        matcher = pattern.matcher(expression);
-        expression = matcher.replaceAll("$3");
-
-        pattern = Pattern.compile("^(\\+*-\\+*-)*\\+(\\+*-\\+*-)*\\+*([\\d(])");
-        matcher = pattern.matcher(expression);
-        expression = matcher.replaceAll("$3");
-
-        return expression;
+    private int getPriority(char operand){
+        if(operand == '+' || operand == '-')
+            return 1;
+        if(operand == '*' || operand == '/')
+            return 2;
+        if(operand == 'p' || operand == 'n')
+            return 3;
+        return 0;
     }
 
-    private boolean checkBadOperands(String expression){
-        char [] expressionCharArray = expression.toCharArray();
-        if(expressionCharArray[0] == '*' || expressionCharArray[0] == '/')
-            return false;
-        for(int i = 1; i < expressionCharArray.length; ++i)
-            if(!Character.isDigit(expressionCharArray[i]) &&
-               expressionCharArray[i] != '(' && expressionCharArray[i] != ')' &&
-               expressionCharArray[i - 1] == expressionCharArray[i])
-                return false;
-        return true;
+    private void addNumber(){
+        numbers.push(currentInteger + currentDecimal);
+        currentInteger = 0;
+        currentDecimal = 0;
+        number = false;
+        point = false;
+    }
+
+    private void addOperand(char operand, char previousSymbol){
+        if(symbols.empty() || getPriority(operand) != 1){
+            symbols.push(operand);
+            return;
+        }
+
+        if(previousSymbol == '+' || previousSymbol == '-' ||
+           previousSymbol == 'p' || previousSymbol =='n'){
+            if(operand == '-') {
+                symbols.pop();
+                switch (previousSymbol) {
+                    case '+':
+                        symbols.push('-');
+                        break;
+                    case '-':
+                        symbols.push('+');
+                        break;
+                    case 'p':
+                        symbols.push('n');
+                        break;
+                    case 'n':
+                        symbols.push('p');
+                }
+            }
+        }else
+            symbols.push(operand);
+    }
+
+
+    private void makeOperation(char operand){
+        double lastNumber = numbers.pop();
+        double previousNumber = 0;
+        if(getPriority(operand) < 3)
+            previousNumber = numbers.pop();
+        switch (operand) {
+            case '+':
+                numbers.push(previousNumber + lastNumber);
+                break;
+            case '-':
+                numbers.push(previousNumber - lastNumber);
+                break;
+            case '*':
+                numbers.push(previousNumber * lastNumber);
+                break;
+            case '/':
+                numbers.push(previousNumber / lastNumber);
+                break;
+            case 'p':
+                numbers.push(lastNumber);
+                break;
+            case 'n':
+                numbers.push(-lastNumber);
+        }
+    }
+
+    private void expandBrackets(){
+        while (symbols.peek() != '(')
+            makeOperation(symbols.pop());
+        symbols.pop();
+    }
+
+    private void expandStack(char operand){
+        while(!symbols.empty() &&
+              getPriority(symbols.peek()) >= getPriority(operand))
+            makeOperation(symbols.pop());
+    }
+
+    private char declSymbol(char symbol, char previousSymbol){
+        if(symbol != '+' && symbol != '-')
+            return symbol;
+        if(!Character.isDigit(previousSymbol) && previousSymbol != ')'){
+            if(symbol == '+')
+                return 'p';
+            return 'n';
+        }
+        return symbol;
+    }
+
+    private double getResult(String expression) throws ParsingException{
+        currentInteger = 0;
+        currentDecimal = 0;
+        point = false;
+        number = false;
+        char previousSymbol = 'x';
+        for(char symbol : expression.toCharArray()){
+            if(Character.isDigit(symbol)) {
+                number = true;
+                if (point)
+                    currentDecimal += (double) Character.getNumericValue(symbol) / 10;
+                else
+                    currentInteger = currentInteger * 10 + Character.getNumericValue(symbol);
+            }else if(symbol == '.') {
+                if (point || !Character.isDigit(previousSymbol))
+                    throw new ParsingException("Invalid expression");
+                point = true;
+            }else {
+                if(number)
+                    addNumber();
+                if(symbol == '(')
+                    symbols.push(symbol);
+                else if (symbol == ')')
+                    expandBrackets();
+                else {
+                    symbol = declSymbol(symbol, previousSymbol);
+                    expandStack(symbol);
+                    addOperand(symbol, previousSymbol);
+                }
+            }
+            previousSymbol = symbol;
+        }
+        if(number)
+            numbers.push(currentInteger + currentDecimal);
+        expandStack('#');
+        return numbers.peek();
     }
 
     @Override
     public double calculate (String expression) throws ParsingException{
         if(expression == null)
             throw  new ParsingException("Null expression");
-        expression = expression.replaceAll(" ", "");
-        expression = expression.replaceAll("\n", "");
+        expression = expression.replaceAll("[\\s]", "");
         if(expression.isEmpty())
             throw new ParsingException("Invalid expression");
-        if(!badSymbolsCheck(expression))
+        if(badSymbolsCheck(expression))
             throw  new ParsingException("Invalid expression");
-        if(!bracketsCheck(expression))
+        if(bracketsCheck(expression))
             throw  new ParsingException("Invalid expression");
-        expression = removeExtraOperands(expression);
-
-
-        return 0;
+      //  System.out.print(expression + "\n");
+        return getResult(expression);
     }
 
-    public static void main(String[] args) throws ParsingException{
-        String s = "-++-+-46-+-+++-(-+23----+4688---+-46-(--68-46)-5)-56-(--+-5)";
-        CalculatorImplementation calc = new CalculatorImplementation();
-        calc.calculate(s);
-
-    }
+//    public static void main(String[] args) throws ParsingException{
+//        String s = " (6.0  ) + \t( - 4) * (  0.0 +\n 5/2)";
+//        CalculatorImplementation calc = new CalculatorImplementation();
+//        System.out.print(calc.calculate(s));
+//    }
 }
 
 
