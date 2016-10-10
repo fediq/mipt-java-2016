cce66ce38e365dff45cd4c6bb7b23c4836f49e29
Alexander Vasilyev <mizabrik@gmail.com>
2016-10-10 04:57:20 +0300

    add unary operators support in task1

--- homework-g597-vasilyev/src/main/java/ru/mipt/java2016/homework/g597/vasilyev/task1/ShuntingYardCalculator.java
+++ homework-g597-vasilyev/src/main/java/ru/mipt/java2016/homework/g597/vasilyev/task1/ShuntingYardCalculator.java
@@ -1,6 +1,5 @@
 package ru.mipt.java2016.homework.g597.vasilyev.task1;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.Stack;
 
@@ -11,7 +10,7 @@ import ru.mipt.java2016.homework.base.task1.ParsingException;
  * Created by mizabrik on 08.10.16.
  * Implementation using Dijkstra shunting algorithm with two stacks.
  */
-public class ShuntingYardCalculator implements Calculator {
+class ShuntingYardCalculator implements Calculator {
     // Calculate expression.
     public double calculate(String expression) throws ParsingException {
         if (expression == null) {
@@ -59,7 +58,7 @@ public class ShuntingYardCalculator implements Calculator {
                 result.add(new OperatorToken(c));
             } else if (c == '(' || c == ')') {
                 if (c == ')' && result.size() > 0 && result.get(result.size() - 1) instanceof BracketToken
-                        && ((BracketToken) result.get(result.size() - 1)).getType() == Bracket.LEFT)
+                        && ((BracketToken) result.get(result.size() - 1)).getType() == Bracket.OPENING)
                     throw new ParsingException("Empty brackets");
                 result.add(new BracketToken(c));
             } else {
@@ -72,37 +71,38 @@ public class ShuntingYardCalculator implements Calculator {
 
     // Get result of expression coded by consequence of tokens.
     private double evaluate(ArrayList<Token> infix) throws ParsingException {
-        ArrayList<Token> result = new ArrayList<>();
         Stack<Token> operators = new Stack<>();
         Stack<Double> numbers = new Stack<>();
         operators.push(new BracketToken('('));
         infix.add(new BracketToken(')'));
         int bracketBalance = 1;
-        boolean gotNumber = false;
-        boolean unaryMinus = false;
+        boolean gotOperand = false;
 
         for (Token token : infix) {
-            if (unaryMinus) {
-                if (token instanceof NumberToken) {
-                    numbers.push(-((NumberToken) token).getNumber());
-                } else {
-                    throw new ParsingException("Unary minus allowed only before number");
-                }
-                unaryMinus = false;
-            } else if (token instanceof NumberToken) {
+            if (token instanceof NumberToken) {
                 numbers.push(((NumberToken) token).getNumber());
-                gotNumber = true;
+                gotOperand = true;
             } else if (token instanceof OperatorToken) {
                 Operator operator = ((OperatorToken) token).getOperator();
-                if (operator == Operator.SUBTRACT && !gotNumber) {
-                    unaryMinus = true;
-                    continue;
+                if (!gotOperand) {
+                    switch (operator) {
+                        case ADD:
+                            operator = Operator.UNARY_PLUS;
+                            break;
+                        case SUBTRACT:
+                            operator = Operator.UNARY_MINUS;
+                            break;
+                        default:
+                            throw new ParsingException("Illegal expression");
+                    }
+                    token = new OperatorToken(operator);
                 }
-                gotNumber = false;
+                gotOperand = false;
                 while (operators.size() > 0 && operators.peek() instanceof OperatorToken) {
                     Operator previousOperator = ((OperatorToken) operators.peek()).getOperator();
-                    if (operatorPriority(previousOperator) <= operatorPriority(operator)) {
-                        applyOperator(numbers, previousOperator);
+                    if (previousOperator.priority < operator.priority
+                            || (operator.hasLeftAssociativity && previousOperator.priority == operator.priority)) {
+                        previousOperator.apply(numbers);
                         operators.pop();
                     } else {
                         break;
@@ -112,19 +112,20 @@ public class ShuntingYardCalculator implements Calculator {
             } else {
                 Bracket bracket = ((BracketToken) token).getType();
                 switch (bracket) {
-                    case LEFT:
+                    case OPENING:
                         operators.push(token);
-                        gotNumber = false;
+                        gotOperand = false;
                         ++bracketBalance;
                         break;
-                    case RIGHT:
+                    case CLOSING:
                         if (bracketBalance == 0)
                             throw new ParsingException("Bad bracket balance");
                         while (operators.peek() instanceof OperatorToken) {
-                            applyOperator(numbers, ((OperatorToken) operators.pop()).getOperator());
+                            ((OperatorToken) operators.pop()).getOperator().apply(numbers);
                         }
                         operators.pop();
                         --bracketBalance;
+                        gotOperand = true;
                         break;
                 }
             }
@@ -138,53 +139,12 @@ public class ShuntingYardCalculator implements Calculator {
         return numbers.pop();
     }
 
-    private int operatorPriority(Operator operator) {
-        switch (operator) {
-            case ADD:
-                return 1;
-            case SUBTRACT:
-                return 1;
-            case MULTIPLY:
-                return 0;
-            case DIVIDE:
-                return 0;
-            default:
-                throw new InvalidParameterException("Srsly?");
-        }
-    }
-
     // Apply operation according to operator to stack of numbers
-    private void applyOperator(Stack<Double> numbers, Operator operator) throws ParsingException {
-        // Too few arguments
-        if (numbers.size() < 2) {
-            throw new ParsingException("Incorrect expression");
-        }
-        double arg2 = numbers.pop();
-        double arg1 = numbers.pop();
-
-        switch (operator) {
-            case ADD:
-                numbers.push(arg1 + arg2);
-                break;
-            case SUBTRACT:
-                numbers.push(arg1 - arg2);
-                break;
-            case MULTIPLY:
-                numbers.push(arg1 * arg2);
-                break;
-            case DIVIDE:
-                numbers.push(arg1 / arg2);
-                break;
-        }
-    }
-
     private boolean isOperatorSymbol(char c) {
         return c == '+' || c == '-' || c == '*' || c == '/';
     }
 
-    private enum Bracket {LEFT, RIGHT}
-
-    private enum Operator {ADD, SUBTRACT, MULTIPLY, DIVIDE}
+    private enum Bracket {OPENING, CLOSING}
 
     private class Token {
     }
@@ -196,7 +156,7 @@ public class ShuntingYardCalculator implements Calculator {
             number = number_;
         }
 
-        public double getNumber() {
+        private double getNumber() {
             return number;
         }
     }
@@ -204,20 +164,20 @@ public class ShuntingYardCalculator implements Calculator {
     private class BracketToken extends Token {
         private Bracket type;
 
-        public BracketToken(char bracket) {
+        private BracketToken(char bracket) {
             switch (bracket) {
                 case '(':
-                    type = Bracket.LEFT;
+                    type = Bracket.OPENING;
                     break;
                 case ')':
-                    type = Bracket.RIGHT;
+                    type = Bracket.CLOSING;
                     break;
                 default:
                     throw new IllegalArgumentException();
             }
         }
 
-        public Bracket getType() {
+        private Bracket getType() {
             return type;
         }
     }
@@ -225,7 +185,11 @@ public class ShuntingYardCalculator implements Calculator {
     private class OperatorToken extends Token {
         private Operator operator;
 
-        public OperatorToken(char operator_) {
+        private OperatorToken(Operator operator) {
+            this.operator = operator;
+        }
+
+        private OperatorToken(char operator_) {
             switch (operator_) {
                 case '+':
                     operator = Operator.ADD;
