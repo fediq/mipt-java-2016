70574d9839c8486ff99a19970ad51de947ed0dc6
topilskiy-ak <syroforce@gmail.com>
2016-10-11 00:20:26 +0300

    Streamlined basic expression-checking Added CalculationSnapshot Added complete_operation Expanded calculate structure to cover more tests Added more comments

--- homework-g595-topilskiy/src/main/java/ru/mipt/java2016/homework/g595/topilskiy/task1/SimpleCalculator.java
+++ homework-g595-topilskiy/src/main/java/ru/mipt/java2016/homework/g595/topilskiy/task1/SimpleCalculator.java
@@ -2,6 +2,7 @@ package ru.mipt.java2016.homework.g595.topilskiy.task1;
 
 import ru.mipt.java2016.homework.base.task1.ParsingException;
 import ru.mipt.java2016.homework.base.task1.Calculator;
+import java.util.Stack;
 
 /**
  * A simple calculator using only basic features.
@@ -9,13 +10,16 @@ import ru.mipt.java2016.homework.base.task1.Calculator;
  * @author Artem K. Topilskiy
  * @since 10.10.16
  */
-public class SimpleCalculator implements Calculator {
+class SimpleCalculator implements Calculator {
+    private final static String ILLEGAL_POSITION_IN_EXPRESSION =
+                                "Expression contains Illegal Symbol Position(s)";
+
     /**
      * Check that expression contains only recognised symbols
-     * and the bracket sequence in expression is correct
+     * and the bracket sequence in expression is correct.
      *
-     * @param expression
-     * @throws ParsingException
+     * @param expression - arithmetic-containing string which correctness is checked
+     * @throws ParsingException - expression is illegal and cannot be calculated
      */
     private void check_expression(String expression) throws ParsingException {
         if (expression == null) {
@@ -28,9 +32,9 @@ public class SimpleCalculator implements Calculator {
 
         int bracket_sum = 0;
         for (int i = 0; bracket_sum >= 0 && i < expression.length(); ++i) {
-            Character c = expression.charAt(i);
+            char c = expression.charAt(i);
 
-            if (!c.isDigit(c)) {
+            if (!Character.isDigit(c)) {
                 switch (c) {
                     case '(':
                         ++bracket_sum;
@@ -59,17 +63,107 @@ public class SimpleCalculator implements Calculator {
     }
 
     /**
-     * Calculate the arithmetic-containing string expression
+     * Class for holding an intermediate calculation.
+     * Mostly for handling intermediate calculation of a bracket element
+     */
+    private class CalculationSnapshot {
+        Double calculation;
+        Character operation;
+
+        CalculationSnapshot(Double calculation_init, Character operation_init) {
+            calculation = calculation_init;
+            operation = operation_init;
+        }
+    }
+
+    /**
+     * A function returns the value of [first] [operation] [second]
+     *
+     * @param first - first operand
+     * @param second - second operand
+     * @param operation - the performed operation
+     * @return Result of [first] [operation] [second]
+     */
+    private Double complete_operation(Double first, Double second, Character operation)
+                                                                throws ParsingException {
+        if (first == null || second == null || operation == null) {
+            throw new ParsingException(ILLEGAL_POSITION_IN_EXPRESSION);
+        }
+
+        Double result = 0.0;
+
+        switch(operation) {
+            case '+':
+                result = first + second;
+                break;
+            case '-':
+                result = first - second;
+                break;
+            case '*':
+                result = first * second;
+                break;
+            case '/':
+                result = first / second;
+                break;
+        }
+
+        return result;
+    }
+
+    /**
+     * Calculate the arithmetic-containing string expression.
      *
      * @param expression - arithmetic-containing string which value is calculated
      * @return calculated expression value
-     * @throws ParsingException
+     * @throws ParsingException - expression is illegal and cannot be calculated
      */
     public double calculate(String expression) throws ParsingException {
         check_expression(expression);
 
-        
+        Stack<CalculationSnapshot> calculation_stack = new Stack<>();
 
-        return 0.0;
+        Character operation = null;
+        Double calculation = null;
+
+        for (int i = 0; i < expression.length(); ++i) {
+            Character c = expression.charAt(i);
+
+            if (c == '(') {
+                calculation_stack.push(new CalculationSnapshot(calculation, operation));
+                calculation = null;
+                operation = null;
+            } else if (c == ')') {
+                CalculationSnapshot last_snapshot = calculation_stack.pop();
+                calculation = complete_operation(calculation, last_snapshot.calculation,
+                                                              last_snapshot.operation);
+                operation = last_snapshot.operation;
+            }
+
+            if (operation != null) {
+                switch (c) {
+                    case '\n':
+                    case '\t':
+                    case ' ':
+                        break;
+                    default:
+                        throw new ParsingException(ILLEGAL_POSITION_IN_EXPRESSION);
+                }
+            }
+            else {
+                switch (c) {
+                    case '\n':
+                    case '\t':
+                    case ' ':
+                        break;
+                    default:
+                        operation = c;
+                }
+            }
+        }
+
+        if (calculation == null) {
+            throw new ParsingException(ILLEGAL_POSITION_IN_EXPRESSION);
+        }
+        return calculation;
     }
 }
