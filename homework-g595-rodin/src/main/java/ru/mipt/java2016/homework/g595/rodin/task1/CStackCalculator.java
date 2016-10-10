689452c8a5b183c37b3009c4aa10a4b58a93a119
Dmitry <kod744@gmail.com>
2016-10-10 17:52:07 +0300

    Merge remote-tracking branch 'upstream/master'
    # Conflicts:
    #	pom.xml

--- homework-g595-rodin/src/main/java/ru/mipt/java2016/homework/g595/rodin/task1/CStackCalculator.java
+++ homework-g595-rodin/src/main/java/ru/mipt/java2016/homework/g595/rodin/task1/CStackCalculator.java
@@ -202,5 +202,4 @@ public class CStackCalculator implements Calculator {
         }
         return 2;
     }
-    
 }
--- homework-g595-rodin/src/test/java/ru/mipt/java2016/homework/g595/rodin/task1/StackCalculatorTest.java
+++ homework-g595-rodin/src/test/java/ru/mipt/java2016/homework/g595/rodin/task1/StackCalculatorTest.java
@@ -1,9 +1,17 @@
-package ru.mipt.java2016.homework.g595.rodin.task1;
-
-/**
+/*
  * Created by Дмитрий on 09.10.16.
  */
-public class StackCalculatorTest extends  CStackCalculator
-{
 
+package ru.mipt.java2016.homework.g595.rodin.task1;
+
+import ru.mipt.java2016.homework.base.task1.Calculator;
+import ru.mipt.java2016.homework.tests.task1.AbstractCalculatorTest;
+
+
+
+public class StackCalculatorTest extends AbstractCalculatorTest {
+    @Override
+    protected Calculator calc() {
+        return new CStackCalculator();
+    }
 }
