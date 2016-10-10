df5f9f43a9a72451c6e31cc427aeb5fa2f92be6f
topilskiy-ak <syroforce@gmail.com>
2016-10-10 23:04:08 +0300

    Added basic expression-checking Added more comments

--- homework-g595-topilskiy/src/main/java/ru/mipt/java2016/homework/g595/topilskiy/task1/SimpleCalculator.java
+++ homework-g595-topilskiy/src/main/java/ru/mipt/java2016/homework/g595/topilskiy/task1/SimpleCalculator.java
@@ -10,14 +10,65 @@ import ru.mipt.java2016.homework.base.task1.Calculator;
  * @since 10.10.16
  */
 public class SimpleCalculator implements Calculator {
+    /**
+     * Check that expression contains only recognised symbols
+     * and the bracket sequence in expression is correct
+     *
+     * @param expression
+     * @throws ParsingException
+     */
     private void check_expression(String expression) throws ParsingException {
         if (expression == null) {
-            throw new ParsingException("Null expression");
+            throw new ParsingException("Expression is Null");
+        }
+
+        if (expression.isEmpty()) {
+            throw new ParsingException("Expression is Empty");
+        }
+
+        int bracket_sum = 0;
+        for (int i = 0; bracket_sum >= 0 && i < expression.length(); ++i) {
+            Character c = expression.charAt(i);
+
+            if (!c.isDigit(c)) {
+                switch (c) {
+                    case '(':
+                        ++bracket_sum;
+                        break;
+                    case ')':
+                        --bracket_sum;
+                        break;
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '\n':
+                    case '\t':
+                    case '.':
+                    case ' ':
+                        break;
+                    default:
+                        throw new ParsingException("Expression contains Illegal Symbol(s)");
+                }
+            }
+        }
+
+        if (bracket_sum != 0) {
+            throw new ParsingException("Expression contains a Faulty Bracket Sequence");
         }
     }
 
+    /**
+     * Calculate the arithmetic-containing string expression
+     *
+     * @param expression - arithmetic-containing string which value is calculated
+     * @return calculated expression value
+     * @throws ParsingException
+     */
     public double calculate(String expression) throws ParsingException {
         check_expression(expression);
+
+        
 
         return 0.0;
     }
