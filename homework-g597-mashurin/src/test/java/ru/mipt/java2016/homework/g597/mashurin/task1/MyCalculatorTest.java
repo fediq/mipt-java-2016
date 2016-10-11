2dcc615bb06552be9ac4b608a2c50db7958b378c
Vlad <27vl@mail.ru>
2016-10-12 02:17:30 +0300

    Completed

---
+++ homework-g597-mashurin/src/test/java/ru/mipt/java2016/homework/g597/mashurin/task1/MyCalculatorTest.java
@@ -0,0 +1,12 @@
+package ru.mipt.java2016.homework.g597.mashurin.task1;
+
+import ru.mipt.java2016.homework.base.task1.Calculator;
+import ru.mipt.java2016.homework.tests.task1.AbstractCalculatorTest;
+
+public class MyCalculatorTest extends AbstractCalculatorTest {
+    @Override
+    protected Calculator calc() {
+        return new ru.mipt.java2016.homework.g597.mashurin.task1.MyCalculator();
+    }
+}
+
