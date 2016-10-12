db30127705dbbec36ea24df0ab3e915e36ec5649
Irina Dmitrieva <atlantie@yandex.ru>
2016-10-12 03:05:32 +0300

    Have tried to solve all of the issues

--- homework-g597-dmitrieva/src/main/java/ru/mipt/java2016/homework/g597/dmitrieva/task1/StackCalculator.java
+++ homework-g597-dmitrieva/src/main/java/ru/mipt/java2016/homework/g597/dmitrieva/task1/StackCalculator.java
@@ -14,33 +14,27 @@ import java.util.TreeSet;
  */
 
 public class StackCalculator implements Calculator{
-    public StringBuilder InfixLine; // Арифметическое выражение в прямой нотации.
-    public StringBuilder PostfixLine; // Арифметическое выражение в обратной нотации.
-    public Double result; // Ответ вычислений.
-    private static Set<Character> OPERATORS = new TreeSet<>(Arrays.asList('+', '-', '*', '/'));
-    private static Set<Character> SYMBOLS = new TreeSet<>(Arrays.asList('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'));
 
-    public StackCalculator () {
-        InfixLine = new StringBuilder();
-        PostfixLine = new StringBuilder();
-        result = new Double(0.0);
-    }
+    private static final Set<Character> SYMBOLS = new TreeSet<>(Arrays.asList
+            ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'));
+    private static final Set<Character> OPERATORS = new TreeSet<>(Arrays.asList('+', '-', '*', '/'));
 
+    @Override
     public double calculate(String expression) throws ParsingException {
-        InfixLine.append(expression);
-        toReversedPolish();
-        calculateReversedPolish();
+        if (expression == null) {
+            throw new ParsingException("The string doesn't exist");
+        }
+        return calculateReversedPolish(toReversedPolish(expression));
 
-        return result;
     }
 
     // Возвращает приоритет операции
     int getPriority(Character operator) throws ParsingException{
-        if (operator.equals('(') || operator.equals(')'))
+        if (operator == '(' || operator ==')')
                 return 0;
-        if (operator.equals('+') || operator.equals('-'))
+        if (operator == '+' || operator == '-')
                 return 1;
-        if (operator.equals('*') || operator.equals('/'))
+        if (operator == '*' || operator == '/')
                 return 2;
         if (operator == '±')
                 return 3;
@@ -48,15 +42,16 @@ public class StackCalculator implements Calculator{
     }
 
     // Переводит инфиксную запись в постфиксную.
-    private void toReversedPolish() throws ParsingException {
+    private String toReversedPolish(String expression) throws ParsingException {
         boolean isUnaryOperation = true;
+        StringBuilder postfixLine = new StringBuilder(); // Арифметическое выражение в обратной нотации.
         Stack<Character> stack = new Stack<>(); // Стек операторов.
         stack.push('(');
-        if ( InfixLine.length() == 0) {
+        if ( expression.length() == 0) {
             throw new ParsingException("The line is empty");
         }
-        for (int i = 0; i < InfixLine.length(); i++) {
-            Character currentSymbol = InfixLine.charAt(i);
+        for (int i = 0; i < expression.length(); i++) {
+            Character currentSymbol = expression.charAt(i);
 
             // Если пробельный символ, то игнориурем.
             if (currentSymbol.equals(' ') || currentSymbol.equals('\t') || currentSymbol.equals('\n')) {
@@ -65,13 +60,13 @@ public class StackCalculator implements Calculator{
 
             //Если символ является цифрой или точкой, то добавляем его к выходной строке.
             if (SYMBOLS.contains(currentSymbol)) {
-                PostfixLine.append(currentSymbol);
+                postfixLine.append(currentSymbol);
                 isUnaryOperation = false;
             }
             // Если символ является открывающей скобкой, помещаем его в стек.
             else if (currentSymbol.equals('(')) {
                 stack.push(currentSymbol);
-                PostfixLine.append(' ').append(' ');
+                postfixLine.append(' ').append(' ');
                 isUnaryOperation = true;
                 //Если символ является оператором
             }
@@ -80,7 +75,7 @@ public class StackCalculator implements Calculator{
                     if (isUnaryOperation) {
                         if (currentSymbol.equals('-')) {
                             stack.push('±');
-                            PostfixLine.append(' ').append(' ');
+                            postfixLine.append(' ').append(' ');
                             isUnaryOperation = false;
                         }
                         else
@@ -92,23 +87,23 @@ public class StackCalculator implements Calculator{
                         while (!stack.empty())
                         {
                             if (getPriority(currentSymbol) <= getPriority(stack.lastElement())) {
-                                PostfixLine.append(' ').append(stack.lastElement()).append(' ');
+                                postfixLine.append(' ').append(stack.lastElement()).append(' ');
                                 stack.pop();
                             }
                             else
                                 break;
                         }
-                        PostfixLine.append(' ').append(' ');
+                        postfixLine.append(' ').append(' ');
                         stack.push(currentSymbol);
                     }
             }
-            // Если символ является закрывающей скобкой: до тех пор, пока верхним элементом стека не станет открывающая скобка,
-            // выталкиваем элементы из стека в выходную строку.
+            // Если символ является закрывающей скобкой: до тех пор, пока верхним элементом
+            // стека не станет открывающая скобка,выталкиваем элементы из стека в выходную строку.
             else if (currentSymbol.equals(')')) {
                 isUnaryOperation = false;
                 while (!stack.empty() && !(stack.lastElement().equals('('))) {
-                    PostfixLine.append(' ');
-                    PostfixLine.append(stack.lastElement()).append(' ');
+                    postfixLine.append(' ');
+                    postfixLine.append(stack.lastElement()).append(' ');
                     stack.pop();
                 }
                 // Если в стеке не осталось открывающейся скобки
@@ -117,24 +112,25 @@ public class StackCalculator implements Calculator{
                     throw new ParsingException("Invalid expression");
                 }
                 stack.pop(); // Убираем из стека соответствующую открывающую скобку.
-                PostfixLine.append(' ').append(' ');
+                postfixLine.append(' ').append(' ');
             } else {
                 throw new ParsingException("Invalid symbol");
             }
         }
         // Когда входная строка закончилась, выталкиваем все символы из стека в выходную строку.
         while (!stack.empty() && !(stack.lastElement().equals('('))) {
-            PostfixLine.append(' ');
-            PostfixLine.append(stack.lastElement()).append(' ');
+            postfixLine.append(' ');
+            postfixLine.append(stack.lastElement()).append(' ');
             stack.pop();
         }
-        PostfixLine.append(' ');
+        postfixLine.append(' ');
         // Если в конце стек остался пуст, то в выражении не согласованы скобки
         // (ибо в начале мы в стек пихали одну открывающую скобку, которая должна была остаться)
         if (stack.empty()) {
             throw new ParsingException("Invalid expression");
         }
         stack.pop(); // Удалим скобку, добавленную в самом начале, если все хорошо.
+        return postfixLine.toString();
     }
 
     //Считает значение элементарного выражения.
@@ -160,17 +156,18 @@ public class StackCalculator implements Calculator{
     }
 
     // Вычисление выражения в постфиксной записи.
-    private void calculateReversedPolish() throws ParsingException {
+    private double calculateReversedPolish(String postfixLine) throws ParsingException {
         Stack<Double> stack = new Stack<>(); // Стек операторов.
         StringBuilder oneNumber = new StringBuilder(); // Для считывания числа из постфиксной строки.
 
-        for (int i = 0; i < PostfixLine.length(); i++) {
-            Character currentSymbol = PostfixLine.charAt(i);
+        for (int i = 0; i < postfixLine.length(); i++) {
+            Character currentSymbol = postfixLine.charAt(i);
+            // Здесь мы, собственно, парсим входную строку
             if (SYMBOLS.contains(currentSymbol)) {
                 oneNumber.append(currentSymbol);
             }
             else {
-                if(i > 0 && currentSymbol.equals(' ') && SYMBOLS.contains(PostfixLine.charAt(i-1))) {
+                if(i > 0 && currentSymbol.equals(' ') && SYMBOLS.contains(postfixLine.charAt(i-1))) {
                     try {
                         stack.push(Double.parseDouble(oneNumber.toString()));
                     } catch (NumberFormatException e) {
@@ -196,9 +193,11 @@ public class StackCalculator implements Calculator{
                 }
             }
         }
+        // В конце в стеке должен был остаться один элемент, который является ответом.
         if (stack.size() == 1) {
-            result = stack.lastElement();
+            return stack.lastElement();
         }
+        // Если нет, то случилось что-то плохое.
         else {
             throw new ParsingException("Invalid expression");
         }
