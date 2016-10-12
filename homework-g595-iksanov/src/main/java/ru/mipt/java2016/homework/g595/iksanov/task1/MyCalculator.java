7da65a83390c4f582989c2d453318c1a80ad567d
Эмиль <iksanov@yahoo.com>
2016-10-12 05:21:14 +0300

    First steps

---
+++ homework-g595-iksanov/src/main/java/ru/mipt/java2016/homework/g595/iksanov/task1/MyCalculator.java
@@ -0,0 +1,17 @@
+package ru.mipt.java2016.homework.g595.iksanov.task1;
+
+import ru.mipt.java2016.homework.base.task1.Calculator;
+import ru.mipt.java2016.homework.base.task1.ParsingException;
+
+/**
+ * Created by Emil Iksanov.
+ */
+public class MyCalculator implements Calculator {
+
+
+    @Override
+    public double calculate(String expression) throws ParsingException {
+
+        return 0;
+    }
+}
