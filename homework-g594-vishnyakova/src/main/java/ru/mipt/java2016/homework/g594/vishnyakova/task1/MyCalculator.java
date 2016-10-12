bfb07a2c94fea9ef8f8677ed565a3ddb964680b3
Nina <ninavivi@mail.ru>
2016-10-12 22:24:39 +0300

    Nina Vishnyakova 594 1st task

---
+++ homework-g594-vishnyakova/src/main/java/ru/mipt/java2016/homework/g594/vishnyakova/task1/MyCalculator.java
@@ -0,0 +1,7 @@
+package ru.mipt.java2016.homework.g594.vishnyakova.task1;
+
+/**
+ * Created by Nina on 11.10.16.
+ */
+public class MyCalculator {
+}
---
+++ homework-g594-vishnyakova/src/test/java/ru/mipt/java2016/homework/g594/vishnuakova/task1/MyCalculatorTest.java
@@ -0,0 +1,7 @@
+package ru.mipt.java2016.homework.g594.vishnuakova.task1;
+
+/**
+ * Created by Nina on 12.10.16.
+ */
+public class MyCalculatorTest {
+}
