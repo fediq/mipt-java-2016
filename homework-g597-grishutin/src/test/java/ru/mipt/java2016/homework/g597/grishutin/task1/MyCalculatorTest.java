e9524400edf1a9a3073cd217e9a5db10f63a1a17
alex <grishutin@179.ru>
2016-10-12 13:12:17 +0300

    Implemented. Now passing all tests except ++1 (which I consider incorrect)

--- homework-g597-grishutin/src/test/java/ru/mipt/java2016/homework/g597/grishutin/task1/MyCalculatorTest.java
+++ homework-g597-grishutin/src/test/java/ru/mipt/java2016/homework/g597/grishutin/task1/MyCalculatorTest.java
@@ -1,17 +1,8 @@
 package ru.mipt.java2016.homework.g597.grishutin.task1;
 
 import ru.mipt.java2016.homework.base.task1.Calculator;
-<<<<<<< e7a36c086b7db12cddb93e239cb5c9ac479c1c69
-import ru.mipt.java2016.homework.base.task1.ParsingException;
 import ru.mipt.java2016.homework.tests.task1.AbstractCalculatorTest;
 
-=======
-import ru.mipt.java2016.homework.tests.task1.AbstractCalculatorTest;
-
-/**
- * Created by Alex on 10.10.2016.
- */
->>>>>>> pom.xml merge for rebase
 public class MyCalculatorTest extends AbstractCalculatorTest {
     @Override
     protected Calculator calc() {
