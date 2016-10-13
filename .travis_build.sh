b1dc97dcb346da4485eaed29bc136c9d6c32f26b
Fedor S. Lavrentyev <fedor.lavrentyev@onefactor.com>
2016-10-14 02:03:12 +0300

    Added second task

--- .travis_build.sh
+++ .travis_build.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [ -z "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     echo "Building push"
     mvn test -B
 else
     echo "Building pull request ${TRAVIS_PULL_REQUEST}"
-    PROJECTS=$(git diff origin/master..HEAD --name-only | grep '/' | sed -e 's/^\([^\/]*\)\/.*$/\1/' | sort | uniq | awk 'BEGIN{ORS=","}; {print $1}')
+    PROJECTS=$(git diff origin/master..HEAD --name-only | grep 'homework-g' | sed -e 's/^\([^\/]*\)\/.*$/\1/' | sort | uniq | awk 'BEGIN{ORS=","}; {print $1}')
     if [ -n "$PROJECTS" ]; then
         echo "Modified modules: ${PROJECTS}"
     	mvn test -B -amd -am -pl ${PROJECTS}
