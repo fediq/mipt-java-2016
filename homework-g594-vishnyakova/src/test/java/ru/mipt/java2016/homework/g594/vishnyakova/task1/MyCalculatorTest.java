7e675e5de3d39e14f0ba4010d9198496a5412414
Nina <ninavivi@mail.ru>
2016-10-13 10:05:03 +0300

    Nina Vishnyakova 594 task 1rename package

--- homework-g594-vishnyakova/src/test/java/ru/mipt/java2016/homework/g594/vishnyakova/task1/MyCalculatorTest.java
+++ homework-g594-vishnyakova/src/test/java/ru/mipt/java2016/homework/g594/vishnyakova/task1/MyCalculatorTest.java
@@ -1,7 +1,6 @@
-package ru.mipt.java2016.homework.g594.vishnuakova.task1;
+package ru.mipt.java2016.homework.g594.vishnyakova.task1;
 
 import ru.mipt.java2016.homework.base.task1.Calculator;
-import ru.mipt.java2016.homework.g594.vishnyakova.task1.MyCalculator;
 import ru.mipt.java2016.homework.tests.task1.AbstractCalculatorTest;
 
 /**
@@ -13,16 +12,3 @@ public class MyCalculatorTest extends AbstractCalculatorTest {
         return new MyCalculator();
     }
 }
-
-
-/*
-
-
-
-public class JEvalCalculatorTest extends AbstractCalculatorTest {
-    @Override
-    protected Calculator calc() {
-        return new JEvalCalculator();
-    }
-}
- */
