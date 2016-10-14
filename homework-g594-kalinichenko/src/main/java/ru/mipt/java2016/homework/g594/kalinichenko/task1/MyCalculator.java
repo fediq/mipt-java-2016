a8e6a035426c3145c4311272bbda33f131eb43d1
Olga Kalinichenko <s15b1_kalinichenko@179.ru>
2016-10-14 20:37:18 +0300

    Codestyle is fixed

--- homework-g594-kalinichenko/src/main/java/ru/mipt/java2016/homework/g594/kalinichenko/task1/MyCalculator.java
+++ homework-g594-kalinichenko/src/main/java/ru/mipt/java2016/homework/g594/kalinichenko/task1/MyCalculator.java
@@ -3,36 +3,36 @@ package ru.mipt.java2016.homework.g594.kalinichenko.task1;
 import ru.mipt.java2016.homework.base.task1.Calculator;
 import ru.mipt.java2016.homework.base.task1.ParsingException;
 import java.util.*;
+
 import static java.lang.Character.*;
 
 class MyCalculator implements Calculator {
-    private interface StackItem
-    {}
-    private interface CalcItem
-    {}
-    private class Bracket implements StackItem
-    {}
-    private class Number implements CalcItem
-    {
+    private interface StackItem { }
+
+    private interface CalcItem { }
+
+    private class Bracket implements StackItem { }
+
+    private class Number implements CalcItem {
         private double value;
-        private Number(double val)
-        {
+
+        private Number(double val) {
             value = val;
         }
-        private double getValue()
-        {
+
+        private double getValue() {
             return value;
         }
     }
-    private enum Operation{ADD, SUB,  MUL, DIV}
-    private class Operator implements CalcItem, StackItem
-    {
+
+    private enum Operation { ADD, SUB, MUL, DIV }
+
+    private class Operator implements CalcItem, StackItem {
         private int priority;
         private Operation operation;
-        private Operator(char c)
-        {
-            switch (c)
-            {
+
+        private Operator(char c) {
+            switch (c) {
                 case '+':
                     operation = Operation.ADD;
                     priority = 2;
@@ -49,12 +49,13 @@ class MyCalculator implements Calculator {
                     operation = Operation.DIV;
                     priority = 1;
                     break;
+                default:
+                    break;
             }
         }
-        private Number calcValue(Number a, Number b)
-        {
-            switch (operation)
-            {
+
+        private Number calcValue(Number a, Number b) {
+            switch (operation) {
                 case ADD:
                     return new Number(a.getValue() + b.getValue());
                 case SUB:
@@ -63,137 +64,99 @@ class MyCalculator implements Calculator {
                     return new Number(a.getValue() * b.getValue());
                 case DIV:
                     return new Number(a.getValue() / b.getValue());
+                default:
+                    break;
             }
             return null;
         }
     }
 
     private ArrayList<CalcItem> getPolishNotation(String expression) throws ParsingException {
-        ArrayList<CalcItem> PolishNotation = new ArrayList<>();
+        ArrayList<CalcItem> polishNotation = new ArrayList<>();
         Stack<StackItem> stack  = new Stack<>();
         boolean unary = true;
         double curNumber = 0;
         double sign = 1;
         boolean prevIsNumber = false;
-        for(int i = 0; i < expression.length(); ++i)
-        {
+        for (int i = 0; i < expression.length(); ++i) {
             Character c = expression.charAt(i);
-            if (isDigit(c) || c.equals('.'))
-            {
-                if (prevIsNumber)
-                {
+            if (isDigit(c) || c.equals('.')) {
+                if (prevIsNumber) {
                     throw new ParsingException("Invalid Expression");
                 }
                 boolean haveDot = false;
-                if (c.equals('.'))
-                {
+                if (c.equals('.')) {
                     haveDot = true;
                 }
-                while(i < expression.length())
-                {
+                while (i < expression.length()) {
                     c = expression.charAt(i);
-                    if (!isDigit(c) && !c.equals('.'))
-                    {
+                    if (!isDigit(c) && !c.equals('.')) {
                         break;
                     }
-                    if (!haveDot)
-                    {
-                        if (isDigit(c))
-                        {
+                    if (!haveDot) {
+                        if (isDigit(c)) {
                             curNumber = curNumber * 10 + getNumericValue(c);
-                        }
-                        else
-                        {
+                        } else {
                             haveDot = true;
                         }
-                    }
-                    else
-                    {
-                        if (isDigit(c))
-                        {
-                            curNumber = curNumber + ((double) getNumericValue(c))/10;
-                        }
-                        else
-                        {
+                    } else {
+                        if (isDigit(c)) {
+                            curNumber = curNumber + ((double) getNumericValue(c)) / 10;
+                        } else {
                             throw new ParsingException("Invalid Expression");
                         }
                     }
                     i++;
                 }
                 i--;
-                PolishNotation.add(new Number(sign * curNumber));
+                polishNotation.add(new Number(sign * curNumber));
                 sign = 1;
                 curNumber = 0;
                 prevIsNumber = true;
                 unary = false;
-            }
-            else if (c.equals('('))
-            {
+            } else if (c.equals('(')) {
                 unary = true;
                 prevIsNumber = false;
                 stack.push(new Bracket());
-            }
-            else if (c.equals(')'))
-            {
-                if (!prevIsNumber)
-                {
+            } else if (c.equals(')')) {
+                if (!prevIsNumber) {
                     throw new ParsingException("Invalid Expression");
                 }
                 unary = false;
                 prevIsNumber = true;
-                while(!stack.empty())
-                {
+                while (!stack.empty()) {
                     StackItem top = stack.peek();
-                    if (top instanceof Operator)
-                    {
-                        PolishNotation.add((Operator) top);
+                    if (top instanceof Operator) {
+                        polishNotation.add((Operator) top);
                         stack.pop();
-                    }
-                    else
-                    {
+                    } else {
                         break;
                     }
                 }
-                if (stack.empty())
-                {
+                if (stack.empty()) {
                     throw new ParsingException("Wrong bracket balance");
-                }
-                else
-                {
+                } else {
                     stack.pop();
                 }
-            }
-            else if (c.equals('+') || c.equals('*') || c.equals('/') || c.equals('-'))
-            {
-                if (unary)
-                {
-                    if (c.equals('-'))
-                    {
+            } else if (c.equals('+') || c.equals('*') || c.equals('/') || c.equals('-')) {
+                if (unary) {
+                    if (c.equals('-')) {
                         sign *= -1;
-                    }
-                    else if (!c.equals('+'))
-                    {
+                    } else if (!c.equals('+')) {
                         throw new ParsingException("Unary * or /");
                     }
                     unary = false;
-                }
-                else
-                {
-                    if (!prevIsNumber)
-                    {
+                } else {
+                    if (!prevIsNumber) {
                         throw new ParsingException("Invalid Expression");
                     }
                     Operator current = new Operator(c);
-                    while(!stack.empty())
-                    {
+                    while (!stack.empty()) {
                         StackItem top = stack.peek();
-                        if (top instanceof Operator && ((Operator) top).priority <= current.priority)
-                        {
-                            PolishNotation.add((Operator) top);
+                        if (top instanceof Operator && ((Operator) top).priority <= current.priority) {
+                            polishNotation.add((Operator) top);
                             stack.pop();
-                        }
-                        else
-                        {
+                        } else {
                             break;
                         }
                     }
@@ -201,45 +164,35 @@ class MyCalculator implements Calculator {
                     unary = true;
                 }
                 prevIsNumber = false;
-            }
-            else if (!isWhitespace(c)) {
+            } else if (!isWhitespace(c)) {
                 throw new ParsingException("Invalid Symbol");
             }
         }
-        while(!stack.empty())
-        {
+        while (!stack.empty()) {
             StackItem top = stack.peek();
-            if (top instanceof Operator)
-            {
-                PolishNotation.add((Operator) top);
+            if (top instanceof Operator) {
+                polishNotation.add((Operator) top);
                 stack.pop();
-            }
-            else
-            {
+            } else {
                 stack.pop();
                 throw new ParsingException("Wrong bracket balance");
             }
         }
-        if (PolishNotation.size() == 0)
-        {
+        if (polishNotation.size() == 0) {
             throw new ParsingException("Empty input");
         }
-        return PolishNotation;
+        return polishNotation;
     }
-    private double getValue(ArrayList<CalcItem> PolishNotation) throws ParsingException
-    {
+
+    private double getValue(ArrayList<CalcItem> polishNotation) throws ParsingException {
         Stack<Number> stack = new Stack<>();
-        for(CalcItem cur:PolishNotation)
-        {
-            if (cur instanceof Number)
-            {
-                stack.push((Number)cur);
-            }
-            else
-            {
+        for (CalcItem cur:polishNotation) {
+            if (cur instanceof Number) {
+                stack.push((Number) cur);
+            } else {
                 Number one = stack.pop();
                 Number two = stack.pop();
-                Number result = ((Operator)cur).calcValue(two, one);
+                Number result = ((Operator) cur).calcValue(two, one);
                 stack.push(result);
             }
         }
@@ -251,11 +204,7 @@ class MyCalculator implements Calculator {
         if (expression == null) {
             throw new ParsingException("NullExpression");
         }
-        ArrayList<CalcItem> PolishNotation = getPolishNotation(expression);
-        return getValue(PolishNotation);
+        ArrayList<CalcItem> polishNotation = getPolishNotation(expression);
+        return getValue(polishNotation);
     }
-
-
-
-
 }
