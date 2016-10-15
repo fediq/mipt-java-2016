ed5a9c08863ba83e2c96166db05ab15d5e99503c
turumtaev <turumtaev.gz@phystech.edu>
2016-10-15 20:00:10 +0300

    Fixe function 'charToInt', 'IsDigit'. replace String with StringBuilder. Add comments

--- homework-g595-turumtaev/src/main/java/ru/mipt/java2016/homework/g595/turumtaev/task1/MyCalculator.java
+++ homework-g595-turumtaev/src/main/java/ru/mipt/java2016/homework/g595/turumtaev/task1/MyCalculator.java
@@ -18,20 +18,16 @@ public class MyCalculator implements Calculator {
         if (expression ==  null) {
             throw new ParsingException("Expression is null");
         }
-        expression = getPostfixLine(expression);
-        return calculateValue(expression);
+        StringBuilder polishExpression; //StringBuider удобнее, потому что я постоянно добавляю в конец строки
+        polishExpression = getPostfixLine(expression); //получим польскую нотацию
+        return calculateValue(polishExpression); //посчитаем значение
     }
 
-    private boolean isDigit(char letter) {
-        return (letter == '0' || letter == '1' || letter == '2' || letter == '3' || letter == '4' ||
-                letter == '5' || letter == '6' || letter == '7' || letter == '8' || letter == '9');
-    }
-
-    private boolean isOperator(char letter) {
+    private boolean isOperator(char letter) { //оператор ли?
         return (letter == '*' || letter == '/' || letter == '+' || letter == '-');
     }
 
-    private int getPriority(char letter) throws ParsingException {
+    private int getPriority(char letter) throws ParsingException { //у операций есть приоритет
         int result = -1;
         if (letter == '(' || letter == ')') {
             result = 0;
@@ -50,34 +46,34 @@ public class MyCalculator implements Calculator {
 
     private boolean isSpace(char letter) {
         return (letter == ' ' || letter ==  '\n' || letter == '\t');
-    }
+    } //на эти знаки не обращаем внимания
 
-    private String getPostfixLine(String expression) throws ParsingException {
-        String resultExpression = "";
-        Stack<Character> operators = new Stack<>();
-        boolean isUnary;
-        isUnary = true;
+    private StringBuilder getPostfixLine(String expression) throws ParsingException {
+        StringBuilder resultExpression = new StringBuilder(); //StringBuilder удобнее
+        Stack<Character> operators = new Stack<>(); //стек для операторов
+        boolean isUnary; //является ли оператор унарным
+        isUnary = true; //если операция в самом начале строки, то она унарна
         for (int i = 0; i < expression.length(); i++) {
-            char letter = expression.charAt(i);
-            if (isOperator(letter)) {
-                resultExpression += ' ';
+            char letter = expression.charAt(i); // берем очередной знак
+            if (isOperator(letter)) { //если он оператор
+                resultExpression.append(' '); // добавим вместо оператора пробел, чтобы числа не склеились
                 if  (isUnary) { //если оператор унарный
-                    if (letter == '+') {
+                    if (letter == '+') { //если унарный плюс, то просто убираем его
                         isUnary = false;
-                    } else if (letter == '-') {
-                        operators.push('~');
+                    } else if (letter == '-') { //если унарный минус, то:
+                        operators.push('~'); // специальный знак, который обозначает унарный минус
                         isUnary = false;
-                    } else {
+                    } else { //других унарных операторов наш калькулятор не понимает
                         throw new ParsingException("Invalid expression");
                     }
                 } else {
-                    isUnary = true;
-                    while (!operators.empty()) {
+                    isUnary = true; //после оператора следующий оператор унарный
+                    while (!operators.empty()) { //пропихнем в строку все операторы большего приоритета
                         char current = operators.pop();
                         if (getPriority(letter) <= getPriority(current)) {
-                            resultExpression += ' ';
-                            resultExpression += current;
-                            resultExpression += ' ';
+                            resultExpression.append(' ');
+                            resultExpression.append(current);
+                            resultExpression.append(' ');
                         } else {
                             operators.push(current);
                             break;
@@ -85,36 +81,36 @@ public class MyCalculator implements Calculator {
                     }
                     operators.push(letter); // Помещаем оператор в стек
                 }
-            } else  if (letter == '(') {
+            } else  if (letter == '(') { //скобку просто помещаем в стек
                 operators.push(letter);
                 isUnary = true;
-            } else if (letter == ')') {
+            } else if (letter == ')') { //если закрывающая скобка, то ищем открывающую
 
                 while (!operators.isEmpty() && operators.peek() != '(') {
-                    resultExpression += ' ';
-                    resultExpression += operators.pop();
-                    resultExpression += ' ';
+                    resultExpression.append(' ');
+                    resultExpression.append(operators.pop());
+                    resultExpression.append(' ');
                 }
                 if (operators.isEmpty()) {
-                    throw new ParsingException("skobe balance error");
+                    throw new ParsingException("skobe balance error"); //если не нашли, то нарушен скобочный баланс
                 }
-                operators.pop();
+                operators.pop(); //вытащим и открывающую
+                isUnary = false; //после скобки оператор бинарный
+            } else if (Character.isDigit(letter) || letter == '.') { //числа просто печатаем в строку
+                resultExpression.append(letter);
                 isUnary = false;
-            } else if (isDigit(letter) || letter == '.') {
-                resultExpression += letter;
-                isUnary = false;
-            } else if (isSpace(letter)) {
+            } else if (isSpace(letter)) { //не реагируем на пробелы и enter
                 continue;
             } else {
-                throw new ParsingException("invalid expression");
+                throw new ParsingException("invalid expression"); //недопустимый знак
             }
         }
         while (!operators.empty()) { // Выталкиваем оставшиеся элементы из стека
             char letter = operators.pop();
             if (isOperator(letter) || letter == '~') {
-                resultExpression += ' ';
-                resultExpression += letter;
-                resultExpression += ' ';
+                resultExpression.append(' ');
+                resultExpression.append(letter);
+                resultExpression.append(' ');
             } else {
                 throw new ParsingException("Invalid expression");
             }
@@ -122,24 +118,24 @@ public class MyCalculator implements Calculator {
         return resultExpression;
     }
 
-    private double calculateValue(String expression) throws ParsingException {
-        Stack<Double> numbers = new Stack<>();
+    private double calculateValue(StringBuilder expression) throws ParsingException {
+        Stack<Double> numbers = new Stack<>(); // стек для промежуточных результатов
         for (int i = 0; i < expression.length(); i++) {
-            char letter = expression.charAt(i);
-            if (isOperator(letter) || letter == '~') {
-                if (letter == '~') {
+            char letter = expression.charAt(i); //очередной знак
+            if (isOperator(letter) || letter == '~') { //если оператор
+                if (letter == '~') { //если унарный минус
                     if (numbers.empty()) {
                         throw new ParsingException("invalid expression");
                     }
                     double number = numbers.pop();
                     numbers.push(-1 * number);
                 } else {
-                    if (numbers.size() < 2) {
+                    if (numbers.size() < 2) { //для бинарного оператора нужно хотя бы 2 операнда
                         throw new ParsingException("invalid expression");
                     }
-                    double firstNumber = numbers.pop();
-                    double secondNumber = numbers.pop();
-                    if (letter ==  '-') {
+                    double firstNumber = numbers.pop(); //первый операнд
+                    double secondNumber = numbers.pop(); //второй операнд
+                    if (letter ==  '-') { //далее проделываем операцию
                         numbers.push(secondNumber - firstNumber);
                     } else if (letter ==  '+') {
                         numbers.push(secondNumber + firstNumber);
@@ -150,60 +146,39 @@ public class MyCalculator implements Calculator {
                     }
                 }
             }
-            if (isDigit(letter)) {
-                boolean isFloatingNumber = false;
-                double result = charToInt(letter);
-                while (isDigit(expression.charAt(i + 1))) {
+            if (Character.isDigit(letter)) { //если цифра
+                boolean isFloatingNumber = false; //встретили ли точку в записи числа
+                double result = charToInt(letter); //из символа получим цифру
+                while (Character.isDigit(expression.charAt(i + 1))) { //если следующий символ тоже цифра
                     i++;
                     result = result * 10 + charToInt(expression.charAt(i));
                 }
-                if (expression.charAt(i + 1) == '.') {
+                if (expression.charAt(i + 1) == '.') { //если точка, переходим к дробной части
                     i++;
                     isFloatingNumber = true;
                 }
-                double fracktion = 1;
-                while (isDigit(expression.charAt(i + 1))) {
+                double fracktion = 1; //на каком мы разряде
+                while (Character.isDigit(expression.charAt(i + 1))) { //пока встречаем цифры
                     i++;
                     fracktion *= 0.1;
                     result = result + fracktion * charToInt(expression.charAt(i));
                 }
-                if (expression.charAt(i + 1) == '.') {
+                if (expression.charAt(i + 1) == '.') { //слишком много точек
                     throw new ParsingException("invalid floating number");
                 }
-                numbers.push(result);
+                numbers.push(result); //в стек
             }
         }
-        if (numbers.size() ==  1) {
+        if (numbers.size() ==  1) { //в конце в стеке должен остаться только конечный ответ
             return numbers.pop();
         } else {
-            throw new ParsingException("Invalid expression");
+            throw new ParsingException("Invalid expression"); //операторов оказалось слишком мало или слишком много
         }
     }
 
     private int charToInt(char letter) throws ParsingException {
-        switch (letter) {
-            case '0':
-                return 0;
-            case '1':
-                return 1;
-            case '2':
-                return 2;
-            case '3':
-                return 3;
-            case '4':
-                return 4;
-            case '5':
-                return 5;
-            case '6':
-                return 6;
-            case '7':
-                return 7;
-            case '8':
-                return 8;
-            case '9':
-                return 9;
-            default:
-                throw new ParsingException("Invalid symbol");
-        }
+        String str = String.valueOf(letter); //мне сказали использовать (2), но оно принимает только строки :\
+        Integer digit = Integer.parseInt(str); //(2), тут ьмираньше была простыня из switch -ей
+        return digit;
     }
 }
