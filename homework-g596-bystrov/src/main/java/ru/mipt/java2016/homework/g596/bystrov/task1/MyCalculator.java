62a185e75c00222736a895cc3cb3d2b15bbf6644
Alex Bystrov <alexbystrov@bitbucket.org>
2016-10-15 23:52:33 +0300

    Minor fixes

--- homework-g596-bystrov/src/main/java/ru/mipt/java2016/homework/g596/bystrov/task1/MyCalculator.java
+++ homework-g596-bystrov/src/main/java/ru/mipt/java2016/homework/g596/bystrov/task1/MyCalculator.java
@@ -15,7 +15,7 @@ public class MyCalculator implements Calculator {
             throw new ParsingException("Null expression");
         }
 
-        StringBuilder exp = new StringBuilder("(" + expression + ")");
+        String exp = "(" + expression + ")";
         try {
             Pair<Double, Integer> answer = brackets(exp, 0);
             return answer.getKey();
@@ -24,92 +24,141 @@ public class MyCalculator implements Calculator {
         }
     }
 
-    private Pair<Double, Integer> toDouble(StringBuilder s, int i) throws ParsingException {
+    private Pair<Double, Integer> toDouble(String exp, int i) throws ParsingException {
         boolean neg = false;
         double ans = 0;
-        while ((s.charAt(i) == ' ') || (s.charAt(i) == '\n') || (s.charAt(i) == '\t')) i++;
-        if (s.charAt(i) == '-') {
+        while ((exp.charAt(i) == ' ') || (exp.charAt(i) == '\n') || (exp.charAt(i) == '\t')) {
+            i++;
+        }
+        if (exp.charAt(i) == '-') {
             neg = true;
             i++;
-            while (s.charAt(i) == ' ') i++;
+            while (exp.charAt(i) == ' ') {
+                i++;
+            }
         }
-        if (s.charAt(i) == '(') {
-            Pair<Double, Integer> temp = brackets(s, i);
-            if (neg) return new Pair<>(-temp.getKey(), temp.getValue());
+        if (exp.charAt(i) == '(') {
+            Pair<Double, Integer> temp = brackets(exp, i);
+            if (neg) {
+                return new Pair<>(-temp.getKey(), temp.getValue());
+            }
             return temp;
-        } else if ((s.charAt(i) < '0') || (s.charAt(i) > '9')) throw new ParsingException("Invalid symbol");
-        while ((i < s.length()) && (s.charAt(i) >= '0') && (s.charAt(i) <= '9')) {
+        } else if (!Character
+                .isDigit(exp.charAt(i))/*(exp.charAt(i) < '0') || (exp.charAt(i) > '9')*/) {
+            throw new ParsingException("Invalid symbol");
+        }
+        while ((i < exp.length()) && (Character
+                .isDigit(exp.charAt(i))/*exp.charAt(i) >= '0') && (exp.charAt(i) <= '9')*/)) {
             ans *= 10;
-            ans += s.charAt(i) - '0';
+            ans += exp.charAt(i) - '0';
             i++;
         }
-        if (s.charAt(i) == '.') {
+        if (exp.charAt(i) == '.') {
             i++;
             int ten = 10;
-            while ((i < s.length()) && (s.charAt(i) >= '0') && (s.charAt(i) <= '9')) {
-                ans += ((s.charAt(i) - '0' + 0.0) / ten);
+            while ((i < exp.length()) && (Character
+                    .isDigit(exp.charAt(i)))/*(exp.charAt(i) >= '0') && (exp.charAt(i) <= '9')*/) {
+                ans += ((exp.charAt(i) - '0' + 0.0) / ten);
                 ten *= 10;
                 i++;
             }
         }
-        while ((s.charAt(i) == ' ') || (s.charAt(i) == '\n') || (s.charAt(i) == '\t')) i++;
-        if (neg) ans = -ans;
+        while ((exp.charAt(i) == ' ') || (exp.charAt(i) == '\n') || (exp.charAt(i) == '\t')) {
+            i++;
+        }
+        if (neg) {
+            ans = -ans;
+        }
         return new Pair<>(ans, i);
     }
 
-    private Pair<Double, Integer> multiplication(StringBuilder s, int i) throws ParsingException {
-        while ((s.charAt(i) == ' ') || (s.charAt(i) == '\n') || (s.charAt(i) == '\t')) i++;
+    private Pair<Double, Integer> multiplication(String exp, int i) throws ParsingException {
+        while ((exp.charAt(i) == ' ') || (exp.charAt(i) == '\n') || (exp.charAt(i) == '\t')) {
+            i++;
+        }
         Pair<Double, Integer> temp;
-        if (s.charAt(i) == '(') temp = brackets(s, i);
-        else temp = toDouble(s, i);
+        if (exp.charAt(i) == '(') {
+            temp = brackets(exp, i);
+        } else {
+            temp = toDouble(exp, i);
+        }
         double ans = temp.getKey();
         i = temp.getValue();
-        while ((i < s.length()) && ((s.charAt(i) == '*') || (s.charAt(i) == '/') || (s.charAt(i) == ' '))) {
-            while ((s.charAt(i) == ' ') || (s.charAt(i) == '\n') || (s.charAt(i) == '\t')) i++;
-            if (s.charAt(i) == '*') {
+        while ((i < exp.length()) && ((exp.charAt(i) == '*') || (exp.charAt(i) == '/') || (
+                exp.charAt(i) == ' '))) {
+            while ((exp.charAt(i) == ' ') || (exp.charAt(i) == '\n') || (exp.charAt(i) == '\t')) {
+                i++;
+            }
+            if (exp.charAt(i) == '*') {
                 Pair<Double, Integer> temp2;
-                if (s.charAt(i + 1) == '(') temp2 = brackets(s, i + 1);
-                else temp2 = toDouble(s, i + 1);
+                if (exp.charAt(i + 1) == '(') {
+                    temp2 = brackets(exp, i + 1);
+                } else {
+                    temp2 = toDouble(exp, i + 1);
+                }
                 i = temp2.getValue();
                 ans *= temp2.getKey();
-            } else if (s.charAt(i) == '/') {
+            } else if (exp.charAt(i) == '/') {
                 Pair<Double, Integer> temp2;
-                if (s.charAt(i + 1) == '(') temp2 = brackets(s, i + 1);
-                else temp2 = toDouble(s, i + 1);
+                if (exp.charAt(i + 1) == '(') {
+                    temp2 = brackets(exp, i + 1);
+                } else {
+                    temp2 = toDouble(exp, i + 1);
+                }
                 i = temp2.getValue();
                 ans /= temp2.getKey();
             }
         }
-        if ((s.length() > i) && ((s.charAt(i) == '+') || (s.charAt(i) == '-') || (s.charAt(i) == ')'))) {
-            while ((s.charAt(i) == ' ') || (s.charAt(i) == '\n') || (s.charAt(i) == '\t')) i++;
+        if ((exp.length() > i) && ((exp.charAt(i) == '+') || (exp.charAt(i) == '-') || (
+                exp.charAt(i) == ')'))) {
+            while ((exp.charAt(i) == ' ') || (exp.charAt(i) == '\n') || (exp.charAt(i) == '\t')) {
+                i++;
+            }
             return new Pair<>(ans, i);
-        } else throw new ParsingException("Invalid symbol");
+        } else {
+            throw new ParsingException("Invalid symbol");
+        }
     }
 
-    private Pair<Double, Integer> sum(StringBuilder s, int i) throws ParsingException {
+    private Pair<Double, Integer> sum(String exp, int i) throws ParsingException {
         double ans = 0;
-        while ((s.charAt(i) == ' ') || (s.charAt(i) == '\n') || (s.charAt(i) == '\t')) i++;
-        Pair<Double, Integer> temp = multiplication(s, i);
+        while ((exp.charAt(i) == ' ') || (exp.charAt(i) == '\n') || (exp.charAt(i) == '\t')) {
+            i++;
+        }
+        Pair<Double, Integer> temp = multiplication(exp, i);
         ans += temp.getKey();
         i = temp.getValue();
-        while ((s.charAt(i) == '+') || (s.charAt(i) == '-') || (s.charAt(i) == ' ')) {
-            while ((s.charAt(i) == ' ') || (s.charAt(i) == '\n') || (s.charAt(i) == '\t')) i++;
-            Pair<Double, Integer> temp2 = multiplication(s, i + 1);
-            if (s.charAt(i) == '+') ans += temp2.getKey();
-            else ans -= temp2.getKey();
+        while ((exp.charAt(i) == '+') || (exp.charAt(i) == '-') || (exp.charAt(i) == ' ')) {
+            while ((exp.charAt(i) == ' ') || (exp.charAt(i) == '\n') || (exp.charAt(i) == '\t')) {
+                i++;
+            }
+            Pair<Double, Integer> temp2 = multiplication(exp, i + 1);
+            if (exp.charAt(i) == '+') {
+                ans += temp2.getKey();
+            } else {
+                ans -= temp2.getKey();
+            }
             i = temp2.getValue();
         }
-        while ((s.charAt(i) == ' ') || (s.charAt(i) == '\n') || (s.charAt(i) == '\t')) i++;
+        while ((exp.charAt(i) == ' ') || (exp.charAt(i) == '\n') || (exp.charAt(i) == '\t')) {
+            i++;
+        }
         return new Pair<>(ans, i);
     }
 
-    private Pair<Double, Integer> brackets(StringBuilder s, int i) throws ParsingException {
-        Pair<Double, Integer> temp = sum(s, i + 1);
-        if (s.charAt(temp.getValue()) == ')') {
-            if (temp.getValue() == i + 1) throw new ParsingException("Empty braces");
-            if ((i == 0) && (temp.getValue() != s.length() - 1)) throw new ParsingException("Too few braces");
+    private Pair<Double, Integer> brackets(String exp, int i) throws ParsingException {
+        Pair<Double, Integer> temp = sum(exp, i + 1);
+        if (exp.charAt(temp.getValue()) == ')') {
+            if (temp.getValue() == i + 1) {
+                throw new ParsingException("Empty braces");
+            }
+            if ((i == 0) && (temp.getValue() != exp.length() - 1)) {
+                throw new ParsingException("Too few braces");
+            }
             int j = temp.getValue();
-            while ((s.charAt(i) == ' ') || (s.charAt(i) == '\n') || (s.charAt(i) == '\t')) j++;
+            while ((exp.charAt(i) == ' ') || (exp.charAt(i) == '\n') || (exp.charAt(i) == '\t')) {
+                j++;
+            }
             return new Pair<>(temp.getKey(), j + 1);
         }
         throw new ParsingException("Too much braces");
