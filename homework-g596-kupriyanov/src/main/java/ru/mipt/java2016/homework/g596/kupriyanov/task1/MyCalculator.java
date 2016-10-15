97d0b3bcfd2b2fcef8d5b52953fed4c69dd38123
Artem Kupriyanov <kupriyanovartem@yandex.ru>
2016-10-16 01:02:34 +0300

    update first task

---
+++ homework-g596-kupriyanov/src/main/java/ru/mipt/java2016/homework/g596/kupriyanov/task1/MyCalculator.java
@@ -0,0 +1,60 @@
+package ru.mipt.java2016.homework.g596.kupriyanov.task1;
+
+/**
+ * Created by Artem Kupriyanov on 12.10.2016.
+ */
+
+import ru.mipt.java2016.homework.base.task1.Calculator;
+import ru.mipt.java2016.homework.base.task1.ParsingException;
+
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class MyCalculator implements Calculator {
+    private static final String CORRECTSYMBOLS = "01234567890.+-*/()";
+
+    private static String makeUnOperations(String inputStr) throws ParsingException {
+        StringBuilder result = new StringBuilder("(");
+        for (int i = 1; i < inputStr.length(); ++i) {
+            char current = inputStr.charAt(i);
+            char previous = inputStr.charAt(i - 1);
+            if (CORRECTSYMBOLS.indexOf(current) == -1) {
+                throw new ParsingException("Incorrect symbol");
+            }
+            if (current == '-' && previous != ')' && !Character.isDigit(previous)) {
+                current = '_';
+            }
+            if (current == ')' && previous == '(') {
+                throw new ParsingException("Empty brackets");
+            }
+            result.append(current);
+        }
+        return result.toString();
+    }
+
+    public double calculate(String inputStr) throws ParsingException {
+        if (inputStr == null) {
+            throw new ParsingException("Null expression");
+        }
+        StringBuilder builder =  new StringBuilder("(" + inputStr.replaceAll("\\s", "") + ")");
+        builder = new StringBuilder(makeUnOperations(builder.toString()));
+        StringTokenizer tokenizer = new StringTokenizer(builder.toString(), "+-*/()_", true);
+        Stack<Number> results = new Stack<Number>();
+        Stack<Operation> operations = new Stack<Operation>();
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            Operation symbol = Operation.fromString(token);
+            symbol.addOperation(results, operations);
+        }
+        if (!operations.isEmpty()) {
+            throw new ParsingException("Bracket balance");
+        }
+        if (results.isEmpty()) {
+            throw new ParsingException("No numbers");
+        }
+        if (results.size() > 1) {
+            throw new ParsingException("Not enough operators");
+        }
+        return results.peek().getValue();
+    }
+}
