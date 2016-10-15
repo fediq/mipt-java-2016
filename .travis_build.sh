05b53ccff06b8edca94924f0ae095e8578657e9a
maximborodin <borodin.ms@phystech.edu>
2016-10-15 23:25:21 +0300

    Fixed travis again

--- .travis_build.sh
+++ .travis_build.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
-
-set -e
-
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    echo "Building push"
-    mvn test -B
-else
-    echo "Building pull request ${TRAVIS_PULL_REQUEST}"
-    PROJECTS=$(git diff origin/master..HEAD --name-only | grep '/' | sed -e 's/^\([^\/]*\)\/.*$/\1/' | sort | uniq | awk 'BEGIN{ORS=","}; {print $1}')
-    if [ -n "$PROJECTS" ]; then
-        echo "Modified modules: ${PROJECTS}"
-    	mvn test -B -amd -am -pl ${PROJECTS}
-    else
-        echo "Cannot detect modified modules; running full test"
-        mvn test -B
-    fi
-fi
+@@ -1,13 +1,25 @@
+ #!/bin/sh
+ 
+ set -e
+ 
+ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
++=======
++#!/bin/bash
++
++set -e
++
++if [ -z "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+     echo "Building push"
+     mvn test -B
+ else
+     echo "Building pull request ${TRAVIS_PULL_REQUEST}"
+     PROJECTS=$(git diff origin/master..HEAD --name-only | grep '/' | sed -e 's/^\([^\/]*\)\/.*$/\1/' | sort | uniq | awk 'BEGIN{ORS=","}; {print $1}')
++=======
