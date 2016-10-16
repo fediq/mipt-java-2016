edec2cc414a1256aea831aec85847a7b7923e6ce
Ilia <irkstepanov@mail.ru>
2016-10-16 22:59:45 +0300

    Deleted unused import

--- homework-g594-stepanov/src/test/java/ru/mipt/java2016/homework/g594/stepanov/task1/CalculatorTestImplementation.java
+++ homework-g594-stepanov/src/test/java/ru/mipt/java2016/homework/g594/stepanov/task1/CalculatorTestImplementation.java
@@ -1,7 +1,6 @@
 package ru.mipt.java2016.homework.g594.stepanov.task1;
 
 import ru.mipt.java2016.homework.base.task1.Calculator;
-import ru.mipt.java2016.homework.g000.lavrentyev.task1.CachedJEvalCalculator;
 import ru.mipt.java2016.homework.tests.task1.AbstractCalculatorTest;
 
 /**
