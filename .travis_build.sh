55d7354dd3d436c91eec20e64bb0d732d09ceedc
maximborodin <borodin.ms@phystech.edu>
2016-10-16 21:00:00 +0300

    Removed useless lines from .travis_build

--- .travis_build.sh
+++ .travis_build.sh
@@ -4,7 +4,6 @@
  set -e
  
  if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-+=======
 +#!/bin/bash
 +
 +set -e
@@ -15,4 +14,3 @@
  else
      echo "Building pull request ${TRAVIS_PULL_REQUEST}"
      PROJECTS=$(git diff origin/master..HEAD --name-only | grep '/' | sed -e 's/^\([^\/]*\)\/.*$/\1/' | sort | uniq | awk 'BEGIN{ORS=","}; {print $1}')
-+=======
