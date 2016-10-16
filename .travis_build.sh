30c54a2e75280591430fe3cd403942255a615c8d
maximborodin <borodin.ms@phystech.edu>
2016-10-17 00:05:45 +0300

    Still experimenting with travis

--- .travis_build.sh
+++ .travis_build.sh
@@ -10,7 +10,7 @@ else
     PROJECTS=$(git diff origin/master..HEAD --name-only | grep 'homework-g' | sed -e 's/^\([^\/]*\)\/.*$/\1/' | sort | uniq | awk 'BEGIN{ORS=","}; {print $1}')
     if [ -n "$PROJECTS" ]; then
         echo "Modified modules: ${PROJECTS}"
-     mvn test -B -amd -am -pl ${PROJECTS}
+    	mvn test -B -amd -am -pl ${PROJECTS}
     else
         echo "Cannot detect modified modules; running full test"
         mvn test -B
