9fc8abb149ac825ef55dc87f49ef7e52a951b1c7
Julia <Julia-st96@mail.ru>
2016-10-16 16:16:03 +0300

    Update2

--- homework-g596-stepanova/src/main/java/ru/mipt/java2016/homework/g596/stepanova/task1/MyCalc.java
+++ homework-g596-stepanova/src/main/java/ru/mipt/java2016/homework/g596/stepanova/task1/MyCalc.java
@@ -13,7 +13,7 @@ import java.util.Set;
 import java.util.Stack;
 
 
-public class MyCalc implements Calculator {
+class MyCalc implements Calculator {
     private static final Set<Character> DOUBLE_DIGITS = Collections.unmodifiableSet(
             new HashSet<>(Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.')));
 
