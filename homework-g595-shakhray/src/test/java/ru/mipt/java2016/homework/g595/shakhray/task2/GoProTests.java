20560d7a93dc23a6dea291fdb480ebfac76433db
riknel <natakir75@gmail.com>
2016-10-28 18:37:13 +0300

    First commit (not working)

---
+++ homework-g595-shakhray/src/test/java/ru/mipt/java2016/homework/g595/shakhray/task2/GoProTests.java
@@ -0,0 +1,26 @@
+package ru.mipt.java2016.homework.g595.shakhray.task2;
+
+import ru.mipt.java2016.homework.base.task2.KeyValueStorage;
+import ru.mipt.java2016.homework.tests.task2.AbstractSingleFileStorageTest;
+import ru.mipt.java2016.homework.tests.task2.Student;
+import ru.mipt.java2016.homework.tests.task2.StudentKey;
+
+/**
+ * Created by Vlad on 26/10/2016.
+ */
+public class GoProTests extends AbstractSingleFileStorageTest {
+    @Override
+    protected KeyValueStorage<String, String> buildStringsStorage(String path) {
+        return new GoProStorage(path, GoProStorage.StorageType.StringStorage);
+    }
+
+    @Override
+    protected KeyValueStorage<Integer, Double> buildNumbersStorage(String path) {
+        return new GoProStorage(path, GoProStorage.StorageType.IntToDoubleStorage);
+    }
+
+    @Override
+    protected KeyValueStorage<StudentKey, Student> buildPojoStorage(String path) {
+        return new GoProStorage(path, GoProStorage.StorageType.StudentsStorage);
+    }
+}
