f08a3a2dcd8f5fc14068ca7e1a8be0b5a8dd3d83
reccy168 <nie1997@yandex.ru>
2016-10-30 01:28:47 +0300

    Refactor RandomAccessFile to DataInput/DataOutput interfaces

--- homework-g595-novikov/src/main/java/ru/mipt/java2016/homework/g595/novikov/task2/MySerialization.java
+++ homework-g595-novikov/src/main/java/ru/mipt/java2016/homework/g595/novikov/task2/MySerialization.java
@@ -3,62 +3,63 @@ package ru.mipt.java2016.homework.g595.novikov.task2;
 import ru.mipt.java2016.homework.tests.task2.Student;
 import ru.mipt.java2016.homework.tests.task2.StudentKey;
 
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.charset.Charset;
 import java.util.Date;
 
 public abstract class MySerialization<T> {
-    public abstract void serialize(RandomAccessFile file, T object) throws IOException;
+    public abstract void serialize(DataOutput file, T object) throws IOException;
 
-    public abstract T deserialize(RandomAccessFile file) throws IOException;
+    public abstract T deserialize(DataInput file) throws IOException;
 
-    protected void serializeString(RandomAccessFile file, String str) throws IOException {
+    protected void serializeString(DataOutput file, String str) throws IOException {
         byte[] strBytes = str.getBytes("UTF-16");
         file.writeInt(strBytes.length);
         file.write(strBytes);
     }
 
-    protected String deserializeString(RandomAccessFile file) throws IOException {
+    protected String deserializeString(DataInput file) throws IOException {
         int strLength = file.readInt();
         byte[] strBytes = new byte[strLength];
         file.readFully(strBytes);
         return new String(strBytes, Charset.forName("UTF-16"));
     }
 
-    protected void serializeInteger(RandomAccessFile file, Integer object) throws IOException {
+    protected void serializeInteger(DataOutput file, Integer object) throws IOException {
         file.writeInt(object.intValue());
     }
 
-    protected Integer deserializeInteger(RandomAccessFile file) throws IOException {
+    protected Integer deserializeInteger(DataInput file) throws IOException {
         return file.readInt();
     }
 
-    protected void serializeDouble(RandomAccessFile file, Double object) throws IOException {
+    protected void serializeDouble(DataOutput file, Double object) throws IOException {
         file.writeDouble(object.doubleValue());
     }
 
-    protected Double deserializeDouble(RandomAccessFile file) throws IOException {
+    protected Double deserializeDouble(DataInput file) throws IOException {
         return file.readDouble();
     }
 
-    protected void serializeBoolean(RandomAccessFile file, Boolean object) throws IOException {
+    protected void serializeBoolean(DataOutput file, Boolean object) throws IOException {
         file.writeBoolean(object.booleanValue());
     }
 
-    protected Boolean deserializeBoolean(RandomAccessFile file) throws IOException {
+    protected Boolean deserializeBoolean(DataInput file) throws IOException {
         return file.readBoolean();
     }
 
-    protected void serializeDate(RandomAccessFile file, Date object) throws IOException {
+    protected void serializeDate(DataOutput file, Date object) throws IOException {
         file.writeLong(object.getTime());
     }
 
-    protected Date deserializeDate(RandomAccessFile file) throws IOException {
+    protected Date deserializeDate(DataInput file) throws IOException {
         return new Date(file.readLong());
     }
 
-    protected void serializeStudent(RandomAccessFile file, Student object) throws IOException {
+    protected void serializeStudent(DataOutput file, Student object) throws IOException {
         serializeInteger(file, object.getGroupId());
         serializeString(file, object.getName());
         serializeString(file, object.getHometown());
@@ -67,7 +68,7 @@ public abstract class MySerialization<T> {
         serializeDouble(file, object.getAverageScore());
     }
 
-    protected Student deserializeStudent(RandomAccessFile file) throws IOException {
+    protected Student deserializeStudent(DataInput file) throws IOException {
         return new Student(deserializeInteger(file),
                 deserializeString(file),
                 deserializeString(file),
@@ -76,12 +77,12 @@ public abstract class MySerialization<T> {
                 deserializeDouble(file));
     }
 
-    protected void serializeStudentKey(RandomAccessFile file, StudentKey object) throws IOException {
+    protected void serializeStudentKey(DataOutput file, StudentKey object) throws IOException {
         serializeInteger(file, object.getGroupId());
         serializeString(file, object.getName());
     }
 
-    protected StudentKey deserializeStudentKey(RandomAccessFile file) throws IOException {
+    protected StudentKey deserializeStudentKey(DataInput file) throws IOException {
         return new StudentKey(deserializeInteger(file),
                 deserializeString(file));
     }
