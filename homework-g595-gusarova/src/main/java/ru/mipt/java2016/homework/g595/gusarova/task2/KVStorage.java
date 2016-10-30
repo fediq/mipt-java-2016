b846efa0b03dafe464bfee23df3d1cb9063899f6
DariaGusarova <daria.gusarova7@yandex.ru>
2016-10-31 01:50:42 +0300

    Fixed problems with passing tests and code style

--- homework-g595-gusarova/src/main/java/ru/mipt/java2016/homework/g595/gusarova/task2/KVStorage.java
+++ homework-g595-gusarova/src/main/java/ru/mipt/java2016/homework/g595/gusarova/task2/KVStorage.java
@@ -15,12 +15,10 @@ public class KVStorage<K, V> implements KeyValueStorage<K, V> {
     private SerializerAndDeserializer<K> serializerAndDeserializerForKey;
     private SerializerAndDeserializer<V> serializerAndDeserializerForValue;
     private HashMap<K, V> map;
-    private Boolean dateInMap;
+    private Boolean dateInMap = true;
 
-    private void AddData() throws IOException{
-        map.clear();
+    private void addData() throws IOException {
         map = new HashMap<K, V>();
-
         DataInputStream input = new DataInputStream(new FileInputStream(f));
         int size = input.readInt();
         for (int i = 0; i < size; i++) {
@@ -28,38 +26,33 @@ public class KVStorage<K, V> implements KeyValueStorage<K, V> {
                     serializerAndDeserializerForValue.deserialize(input));
         }
         input.close();
-        dateInMap = true;
     }
 
     public KVStorage(String path, SerializerAndDeserializer<K> forKey,
-                     SerializerAndDeserializer<V> forValue) throws IOException
-    {
+                     SerializerAndDeserializer<V> forValue) throws IOException {
         f = new File(path);
         if (!f.exists() || !f.isDirectory()) {
             throw new IOException("this path is incorrect");
         }
-        f = new File(path + "storage.txt");
+        f = new File(path + "/storage.txt");
         serializerAndDeserializerForKey = forKey;
         serializerAndDeserializerForValue = forValue;
-        AddData();
-
-    }
-
-
-    private Boolean checkOpen() {
-        if (!dateInMap) {
-            System.out.println("base is closed");
-            return false;
+        try {
+            addData();
+        } catch (IOException exp) {
+            //base was never printed on disk
         }
-        return true;
+
     }
+
+
+
     @Override
-    public V read(K key){
-        if (!checkOpen()) {
-            return null;
+    public V read(K key) {
+        if (!dateInMap) {
+            throw new RuntimeException("base closed");
         }
         if (!map.containsKey(key)) {
-            System.out.println("invalid key");
             return null;
         } else {
             return map.get(key);
@@ -68,19 +61,19 @@ public class KVStorage<K, V> implements KeyValueStorage<K, V> {
 
     @Override
     public boolean exists(K key) {
-        if (!checkOpen()) {
-            return false;
+        if (!dateInMap) {
+            throw new RuntimeException("base closed");
         }
         return map.containsKey(key);
     }
 
     @Override
     public void write(K key, V value) {
-        if (!checkOpen()) {
-            return;
+        if (!dateInMap) {
+            throw new RuntimeException("base closed");
         }
         if (map.containsKey(key)) {
-            System.out.println("invalid key");
+            map.replace(key, value);
         } else {
             map.put(key, value);
         }
@@ -88,29 +81,26 @@ public class KVStorage<K, V> implements KeyValueStorage<K, V> {
 
     @Override
     public void delete(K key) {
-        if (!checkOpen()) {
-            return;
+        if (!dateInMap) {
+            throw new RuntimeException("base closed");
         }
         if (map.containsKey(key)) {
             map.remove(key);
-        }
-        else {
-            System.out.println("there is no object with such key");
         }
     }
 
     @Override
     public Iterator<K> readKeys() {
-        if (!checkOpen()) {
-            return null;
+        if (!dateInMap) {
+            throw new RuntimeException("base closed");
         }
         return map.keySet().iterator();
     }
 
     @Override
     public int size() {
-        if (!checkOpen()) {
-            return 0;
+        if (!dateInMap) {
+            throw new RuntimeException("base closed");
         }
         return map.size();
     }
@@ -118,7 +108,7 @@ public class KVStorage<K, V> implements KeyValueStorage<K, V> {
     @Override
     public void close() throws IOException {
         if (!dateInMap) {
-            return;
+            throw new RuntimeException("base closed");
         }
         try {
             DataOutputStream output = new DataOutputStream(new FileOutputStream(f));
@@ -129,7 +119,7 @@ public class KVStorage<K, V> implements KeyValueStorage<K, V> {
             }
             output.close();
             dateInMap = false;
-        } catch(IOException exp) {
+        } catch (IOException exp) {
             System.out.println(exp.getMessage());
         }
     }
--- homework-g595-gusarova/src/test/java/ru/mipt/java2016/homework/g595/gusarova/task2/KVStorageTests.java
+++ homework-g595-gusarova/src/test/java/ru/mipt/java2016/homework/g595/gusarova/task2/KVStorageTests.java
@@ -1,24 +1,16 @@
 package ru.mipt.java2016.homework.g595.gusarova.task2;
 
-import org.junit.Ignore;
-import org.junit.Test;
 import ru.mipt.java2016.homework.base.task2.KeyValueStorage;
 import ru.mipt.java2016.homework.tests.task2.AbstractSingleFileStorageTest;
 import ru.mipt.java2016.homework.tests.task2.Student;
 import ru.mipt.java2016.homework.tests.task2.StudentKey;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by Дарья on 30.10.2016.
  */
 public class KVStorageTests extends AbstractSingleFileStorageTest {
-    private final Map<String, Map<String, String>> stringMaps = new ConcurrentHashMap<>();
-    private final Map<String, Map<Integer, Double>> numbersMaps = new ConcurrentHashMap<>();
-    private final Map<String, Map<StudentKey, Student>> pojoMaps = new ConcurrentHashMap<>();
-
     @Override
     protected KeyValueStorage<String, String> buildStringsStorage(String path) {
         KVStorage<String, String> temp;
@@ -56,12 +48,5 @@ public class KVStorageTests extends AbstractSingleFileStorageTest {
             return null;
         }
         return temp;
-    }
-
-    @Override
-    @Test
-    @Ignore
-    public void testPersistAndCopy() {
-        super.testPersistAndCopy();
     }
 }
