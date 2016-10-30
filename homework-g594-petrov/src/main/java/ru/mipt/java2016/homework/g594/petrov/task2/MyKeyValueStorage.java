6f46c03d6f5352d9e4636596d66c58daeb43700b
Petrov Filipp <fileasfog931997@gmail.com>
2016-10-30 23:37:35 +0300

    Made key value storage. Seems to be working.

--- homework-g594-petrov/src/main/java/ru/mipt/java2016/homework/g594/petrov/task2/MyKeyValueStorage.java
+++
@@ -1,7 +0,0 @@
-package ru.mipt.java2016.homework.g594.petrov.task2;
-
-/**
- * Created by philipp on 30.10.16.
- */
-public class MyKeyValueStorage {
-}
---
+++ homework-g594-petrov/src/main/java/ru/mipt/java2016/homework/g594/petrov/task2/SerializeString.java
@@ -0,0 +1,27 @@
+package ru.mipt.java2016.homework.g594.petrov.task2;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+/**
+ * Created by philipp on 30.10.16.
+ */
+public class SerializeString implements InterfaceSerialization<String> {
+    @Override
+    public String readValue(DataInputStream inputStream) throws IllegalStateException {
+        try {
+            return inputStream.readUTF();
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e.getCause());
+        }
+    }
+
+    @Override
+    public void writeValue(String obj, DataOutputStream outputStream) throws IllegalStateException {
+        try {
+            outputStream.writeUTF(obj);
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e.getCause());
+        }
+    }
+}
