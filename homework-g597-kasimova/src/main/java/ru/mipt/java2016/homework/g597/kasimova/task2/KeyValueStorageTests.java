2d76025a8abe847ae49ab03e8c977187aa0ce428
Kasimova Nadezhda <nadezhda_kasimova@mail.ru>
2016-10-30 23:50:12 +0300

    Second task with some fix

--- homework-g597-kasimova/src/main/java/ru/mipt/java2016/homework/g597/kasimova/task2/KeyValueStorageTests.java
+++ homework-g597-kasimova/src/main/java/ru/mipt/java2016/homework/g597/kasimova/task2/KeyValueStorageTests.java
@@ -11,19 +11,16 @@ import ru.mipt.java2016.homework.tests.task2.StudentKey;
 public class KeyValueStorageTests extends AbstractSingleFileStorageTest {
     @Override
     protected KeyValueStorage<String, String> buildStringsStorage(String path) {
-        return new MKeyValueStorage<String, String>(path, MSerialization.STRING_SERIALIZER,
-                MSerialization.STRING_SERIALIZER);
+        return new MKeyValueStorage<String, String>(path, MSerialization.stringSerializer, MSerialization.stringSerializer);
     }
 
     @Override
     protected KeyValueStorage<Integer, Double> buildNumbersStorage(String path) {
-        return new MKeyValueStorage<Integer, Double>(path, MSerialization.INTEGER_SERIALIZER,
-                MSerialization.DOUBLE_SERIALIZER);
+        return new MKeyValueStorage<Integer, Double>(path, MSerialization.integerSerializer, MSerialization.doubleSerializer);
     }
 
     @Override
     protected KeyValueStorage<StudentKey, Student> buildPojoStorage(String path) {
-        return new MKeyValueStorage<StudentKey, Student>(path, MSerialization.STUDENT_KEY_SERIALIZER,
-                MSerialization.STUDENT_SERIALIZER);
+        return new MKeyValueStorage<StudentKey, Student>(path, MSerialization.studentKeySerializer, MSerialization.studentSerializer);
     }
 }
