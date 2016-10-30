6f2f20542b0f99cdba24b88e97ae0b0e808d3675
Evgeniy Egorov <evgeniy.eg1@yandex.ru>
2016-10-30 23:03:56 +0300

    MyKeyValueStorage finally completed!!!

---
+++ homework-g596-egorov/src/main/java/ru/mipt/java2016/homework/g596/egorov/task2/serializers/SerializerofDouble.java
@@ -0,0 +1,21 @@
+package ru.mipt.java2016.homework.g596.egorov.task2.serializers;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by евгений on 30.10.2016.
+ */
+public class SerializerofDouble implements SerializerInterface<Double> {
+
+    @Override
+    public Double deserialize(DataInputStream rd) throws IOException {
+        return rd.readDouble();
+    }
+
+    @Override
+    public void serialize(DataOutputStream wr, Double obj) throws IOException {
+        wr.writeDouble(obj);
+    }
+}
