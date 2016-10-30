00e42bd5c437a1b6dae7008b0f8e1dcf60c5f285
Julia Ivanova <ivanova-j-ulia@yandex.ru>
2016-10-31 00:26:11 +0300

    Add key-value storage implementation

---
+++ homework-g596-ivanova/src/main/java/ru/mipt/java2016/homework/g596/ivanova/task2/BooleanSerialisation.java
@@ -0,0 +1,37 @@
+package ru.mipt.java2016.homework.g596.ivanova.task2;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * Created by julia on 30.10.16.
+ */
+public final class BooleanSerialisation implements Serialisation<Boolean> {
+    /**
+     * Instance of current class.
+     */
+    private static BooleanSerialisation instance = new BooleanSerialisation();
+
+    /**
+     * Constructor for the class.
+     */
+    private BooleanSerialisation() { }
+
+    /**
+     * @return instance of current class.
+     */
+    public static BooleanSerialisation getInstance() {
+        return instance;
+    }
+
+    @Override
+    public Boolean read(final DataInput file) throws IOException {
+        return file.readBoolean();
+    }
+
+    @Override
+    public void write(final DataOutput file, final Boolean object) throws IOException {
+        file.writeBoolean(object);
+    }
+}
