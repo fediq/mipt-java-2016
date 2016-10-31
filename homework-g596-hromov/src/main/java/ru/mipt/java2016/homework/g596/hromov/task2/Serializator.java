134c0db4190f7e5f6c22a99d69635be32244854d
Igor Hromov <hromov.igor@gmail.com>
2016-11-01 00:52:37 +0300

    Edited codestyle

--- homework-g596-hromov/src/main/java/ru/mipt/java2016/homework/g596/hromov/task2/Serializator.java
+++ homework-g596-hromov/src/main/java/ru/mipt/java2016/homework/g596/hromov/task2/Serializator.java
@@ -2,14 +2,15 @@ package ru.mipt.java2016.homework.g596.hromov.task2;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 
 /**
  * Created by igorhromov on 30.10.16.
  */
 interface Serializator<type> {
 
-    void serializeToStream(type value, DataOutputStream outStream);
+    void serializeToStream(type value, DataOutputStream outStream) throws IOException;
 
-    type deserializeFromStream(DataInputStream inputStream);
+    type deserializeFromStream(DataInputStream inputStream) throws IOException;
 
 }
