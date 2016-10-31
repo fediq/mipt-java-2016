10bd0bcb60a757797def0cb5a40363cb196e2c23
Ильдар <fattakhetdinov@mail.ru>
2016-10-31 16:21:14 +0300

    Убрал реализацию инетрфейса из интерфейса

--- homework-g596-fattakhetdinov/src/main/java/ru/mipt/java2016/homework/g596/fattakhetdinov/task2/SerializationStrategy.java
+++ homework-g596-fattakhetdinov/src/main/java/ru/mipt/java2016/homework/g596/fattakhetdinov/task2/SerializationStrategy.java
@@ -11,54 +11,9 @@ public interface SerializationStrategy<T> {
 
     String getType(); //Возвращает тип стратегии сериализации
 
-    SerializationStrategy<String> STRING_SERIALIZATOR = new SerializationStrategy<String>() {
-        @Override
-        public void serializeToFile(String str, DataOutputStream output) throws IOException {
-            output.writeUTF(str);
-        }
 
-        @Override
-        public String deserializeFromFile(DataInputStream input) throws IOException {
-            return input.readUTF();
-        }
 
-        @Override
-        public String getType() {
-            return "String";
-        }
-    };
 
-    SerializationStrategy<Integer> INTEGER_SERIALIZATOR = new SerializationStrategy<Integer>() {
-        @Override
-        public void serializeToFile(Integer value, DataOutputStream output) throws IOException {
-            output.writeInt(value);
-        }
 
-        @Override
-        public Integer deserializeFromFile(DataInputStream input) throws IOException {
-            return input.readInt();
-        }
 
-        @Override
-        public String getType() {
-            return "Integer";
-        }
-    };
-
-    SerializationStrategy<Double> DOUBLE_SERIALIZATOR = new SerializationStrategy<Double>() {
-        @Override
-        public void serializeToFile(Double value, DataOutputStream output) throws IOException {
-            output.writeDouble(value);
-        }
-
-        @Override
-        public Double deserializeFromFile(DataInputStream input) throws IOException {
-            return input.readDouble();
-        }
-
-        @Override
-        public String getType() {
-            return "Double";
-        }
-    };
 }
--- homework-g596-fattakhetdinov/src/test/java/ru/mipt/java2016/homework/g596/fattakhetdinov/task2/MyFileStorageTest.java
+++ homework-g596-fattakhetdinov/src/test/java/ru/mipt/java2016/homework/g596/fattakhetdinov/task2/MyFileStorageTest.java
@@ -11,61 +11,16 @@ import ru.mipt.java2016.homework.tests.task2.StudentKey;
 
 public class MyFileStorageTest extends AbstractSingleFileStorageTest {
 
-    private static SerializationStrategy<StudentKey> STUDENT_KEY_SERIALIZATOR
-            = new SerializationStrategy<StudentKey>() {
-        @Override
-        public void serializeToFile(StudentKey studentKey, DataOutputStream output) throws IOException {
-            output.writeInt(studentKey.getGroupId());
-            output.writeUTF(studentKey.getName());
-        }
 
-        @Override
-        public StudentKey deserializeFromFile(DataInputStream input) throws IOException {
-            int groupId = input.readInt();
-            String name = input.readUTF();
-            return new StudentKey(groupId, name);
-        }
 
-        @Override
-        public String getType() {
-            return "StudentKey";
-        }
-    };
-
-    private static SerializationStrategy<Student> STUDENT_SERIALIZATOR = new SerializationStrategy<Student>() {
-        @Override
-        public void serializeToFile(Student student, DataOutputStream output) throws IOException {
-            output.writeInt(student.getGroupId());
-            output.writeUTF(student.getName());
-            output.writeUTF(student.getHometown());
-            output.writeLong(student.getBirthDate().getTime());
-            output.writeBoolean(student.isHasDormitory());
-            output.writeDouble(student.getAverageScore());
-        }
-
-        @Override
-        public Student deserializeFromFile(DataInputStream input) throws IOException {
-            int groupId = input.readInt();
-            String name = input.readUTF();
-            String hometown = input.readUTF();
-            Date birthDate = new Date(input.readLong());
-            boolean hasDormitory = input.readBoolean();
-            double averageScore = input.readDouble();
-            return new Student(groupId, name, hometown, birthDate, hasDormitory, averageScore);
-        }
-
-        @Override
-        public String getType() {
-            return "Student";
-        }
-    };
 
     @Override
     protected KeyValueStorage<String, String> buildStringsStorage(String path) {
         KeyValueStorage<String, String> result = null;
         try {
-            result = new MyKeyValueStorage<>(path, SerializationStrategy.STRING_SERIALIZATOR,
-                    SerializationStrategy.STRING_SERIALIZATOR);
+            result = new MyKeyValueStorage<>(path,
+                    new StringSerializator(),
+                    new StringSerializator());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -76,8 +31,9 @@ public class MyFileStorageTest extends AbstractSingleFileStorageTest {
     protected KeyValueStorage<Integer, Double> buildNumbersStorage(String path) {
         KeyValueStorage<Integer, Double> result = null;
         try {
-            result = new MyKeyValueStorage<>(path, SerializationStrategy.INTEGER_SERIALIZATOR,
-                    SerializationStrategy.DOUBLE_SERIALIZATOR);
+            result = new MyKeyValueStorage<>(path,
+                    new IntegerSerializator(),
+                    new DoubleSerializator());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -88,8 +44,9 @@ public class MyFileStorageTest extends AbstractSingleFileStorageTest {
     protected KeyValueStorage<StudentKey, Student> buildPojoStorage(String path) {
         KeyValueStorage<StudentKey, Student> result = null;
         try {
-            result = new MyKeyValueStorage<>(path, STUDENT_KEY_SERIALIZATOR,
-                    STUDENT_SERIALIZATOR);
+            result = new MyKeyValueStorage<>(path,
+                    new StudentKeySerializator(),
+                    new StudentSerializator());
         } catch (IOException e) {
             e.printStackTrace();
         }
