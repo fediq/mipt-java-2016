bd94b38a0ef21077e24bc722dec339141dbd32fe
Alexander Gorelick <gorelickalexander@gmail.com>
2016-10-31 23:57:38 +0300

    Reclone

---
+++ homework-g594-gorelick/src/main/java/ru/mipt/java2016/homework/g594/gorelick/task2/RandomAccessFileManager.java
@@ -0,0 +1,7 @@
+package ru.mipt.java2016.homework.g594.gorelick.task2;
+
+/**
+ * Created by alex on 10/31/16.
+ */
+public class RandomAccessFileManager {
+}
---
+++ homework-g594-gorelick/src/main/java/ru/mipt/java2016/homework/g594/gorelick/task2/Serializer.java
@@ -0,0 +1,8 @@
+package ru.mipt.java2016.homework.g594.gorelick.task2;
+
+/**
+ * Created by Alexander Gorelick on 10/31/16.
+ */
+public class Serializer<V> {
+     vserialize() {}
+}
