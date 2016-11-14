096178a07871d579ebde0f162e8426bbfe30d825
Clumpy Tuna <anuhinm@mail.ru>
2016-11-14 19:16:43 +0300

    undo

--- homework-g594-anukhin/src/main/java/ru/mipt/java2016/homework/g594/anukhin/task2/Serializable.java
+++ homework-g594-anukhin/src/main/java/ru/mipt/java2016/homework/g594/anukhin/task2/Serializable.java
@@ -8,7 +8,9 @@ import java.io.IOException;
  * Created by clumpytuna on 29.10.16.
  */
 public interface Serializable<T> {
+    /* Пишет объект типа Т в поток*/
     void serialize(DataOutputStream input, T obj) throws IOException;
 
+    /* Возращает объект типа T из потока*/
     T deserialize(DataInputStream output) throws IOException;
 }
