6f5972d5cfe3b0f028681cc8bb14a39cab7182c6
wheeltune <wheeltune@gmail.com>
2016-11-20 02:15:28 +0300

    Test RandomAccess

--- homework-g594-krokhalev/src/main/java/ru/mipt/java2016/homework/g594/krokhalev/task3/StorageReader.java
+++ homework-g594-krokhalev/src/main/java/ru/mipt/java2016/homework/g594/krokhalev/task3/StorageReader.java
@@ -2,6 +2,7 @@ package ru.mipt.java2016.homework.g594.krokhalev.task3;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
 
 public class StorageReader<K, V> {
     public static final int BOOL_SIZE = 1;
@@ -23,8 +24,23 @@ public class StorageReader<K, V> {
             size -= get;
         }
     }
+    private void readAll(RandomAccessFile stream, byte[] buff, int offset, int size) throws IOException {
+        while (size > 0) {
+            int get = stream.read(buff, offset, size);
+            offset += get;
+            size -= get;
+        }
+    }
 
     public int readInt(InputStream stream) throws IOException {
+        byte[] intBuff = new byte[INT_SIZE];
+
+        readAll(stream, intBuff, 0, INT_SIZE);
+
+        return (int) Serializer.deserialize(int.class, intBuff);
+    }
+
+    public int readInt(RandomAccessFile stream) throws IOException {
         byte[] intBuff = new byte[INT_SIZE];
 
         readAll(stream, intBuff, 0, INT_SIZE);
@@ -41,6 +57,11 @@ public class StorageReader<K, V> {
     }
 
     public byte[] readItem(InputStream stream) throws IOException {
+        int size = readInt(stream);
+        return readBytes(stream, size);
+    }
+
+    public byte[] readItem(RandomAccessFile stream) throws IOException {
         int size = readInt(stream);
         return readBytes(stream, size);
     }
@@ -65,12 +86,23 @@ public class StorageReader<K, V> {
 
         return keyBuff;
     }
+    public byte[] readBytes(RandomAccessFile stream, int size) throws IOException {
+
+        byte[] keyBuff = new byte[size];
+
+        readAll(stream, keyBuff, 0, size);
+
+        return keyBuff;
+    }
 
     public K readKey(InputStream stream) throws IOException {
         return (K) Serializer.deserialize(keyClass, readItem(stream));
     }
 
     public V readValue(InputStream stream) throws IOException {
+        return (V) Serializer.deserialize(valueClass, readItem(stream));
+    }
+    public V readValue(RandomAccessFile stream) throws IOException {
         return (V) Serializer.deserialize(valueClass, readItem(stream));
     }
 
