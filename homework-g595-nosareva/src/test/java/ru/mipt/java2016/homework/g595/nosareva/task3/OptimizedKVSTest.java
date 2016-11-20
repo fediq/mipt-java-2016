9c28645dad92f3209b0734539fe4333b27819b69
MariaNosareva <mashunya_19-97@mail.ru>
2016-11-20 17:47:09 +0300

    Renamed test files

--- homework-g595-nosareva/src/test/java/ru/mipt/java2016/homework/g595/nosareva/task3/OptimizedKVSTest.java
+++ homework-g595-nosareva/src/test/java/ru/mipt/java2016/homework/g595/nosareva/task3/OptimizedKVSTest.java
@@ -1,25 +1,28 @@
-package ru.mipt.java2016.homework.g595.nosareva.task2;
+package ru.mipt.java2016.homework.g595.nosareva.task3;
 
 import ru.mipt.java2016.homework.base.task2.KeyValueStorage;
-import ru.mipt.java2016.homework.tests.task2.AbstractSingleFileStorageTest;
+import ru.mipt.java2016.homework.g595.nosareva.task2.*;
 import ru.mipt.java2016.homework.tests.task2.Student;
 import ru.mipt.java2016.homework.tests.task2.StudentKey;
+import ru.mipt.java2016.homework.tests.task3.KeyValueStoragePerformanceTest;
 
+import javax.xml.bind.ValidationException;
 import java.io.IOException;
 
 /**
- * Created by maria on 26.10.16.
+ * Created by maria on 19.11.16.
  */
-public class KVStest extends AbstractSingleFileStorageTest {
-
+public class OptimizedKVSTest extends KeyValueStoragePerformanceTest {
     @Override
     protected KeyValueStorage<String, String> buildStringsStorage(String path) {
-        KVStorage<String, String> res = null;
+        OptimizedKVStorage<String, String> res = null;
         try {
-            res = new KVStorage<>(path, new SerializerForString(),
+            res = new OptimizedKVStorage<>(path, new SerializerForString(),
                     new SerializerForString());
         } catch (IOException excep) {
             System.out.println(excep.getMessage());
+        } catch (ValidationException exc) {
+            System.out.println(exc.getMessage());
         }
         return res;
     }
@@ -27,27 +30,30 @@ public class KVStest extends AbstractSingleFileStorageTest {
 
     @Override
     protected KeyValueStorage<Integer, Double> buildNumbersStorage(String path) {
-        KVStorage<Integer, Double> res = null;
+        OptimizedKVStorage<Integer, Double> res = null;
         try {
-            res = new KVStorage<>(path, new SerializerForInteger(),
+            res = new OptimizedKVStorage<>(path, new SerializerForInteger(),
                     new SerializerForDouble());
         } catch (IOException excep) {
             System.out.println(excep.getMessage());
+        } catch (ValidationException exc) {
+            System.out.println(exc.getMessage());
         }
         return res;
     }
 
     @Override
     protected KeyValueStorage<StudentKey, Student> buildPojoStorage(String path) {
-        KVStorage<StudentKey, Student> res = null;
+        OptimizedKVStorage<StudentKey, Student> res = null;
         try {
-            res = new KVStorage<>(path, new SerializerForStudentKey(),
+            res = new OptimizedKVStorage<>(path, new SerializerForStudentKey(),
                     new SerializerForStudent());
         } catch (IOException excep) {
             System.out.println(excep.getMessage());
+        } catch (ValidationException exc) {
+            System.out.println(exc.getMessage());
         }
         return res;
     }
-
-
 }
+
