9281b01415829263903a3ba96587ef9c1b86e18d
VadimPl <men19977@mail.ru>
2016-11-21 23:58:49 +0300

    3 task

--- homework-tests/src/main/java/ru/mipt/java2016/homework/tests/task3/KeyValueStoragePerformanceTest.java
+++ homework-tests/src/main/java/ru/mipt/java2016/homework/tests/task3/KeyValueStoragePerformanceTest.java
@@ -149,7 +149,7 @@ public abstract class KeyValueStoragePerformanceTest extends AbstractSingleFileS
     }
 
     // Вы можете пометить этот тест как @Ignored во время отладки, если он занимает у вас слишком много времени
-   @Test
+    @Test
     public void measure100kWDump100kR() {
         AtomicLong summaryWriteTime = new AtomicLong(0L);
         AtomicLong summaryReadTime = new AtomicLong(0L);
