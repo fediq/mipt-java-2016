94e8f36b8e732676d5611ceb4dd0d03c7265c01c
Ulyanin Dmitry <ulyanin1997@gmail.com>
2016-11-21 23:59:03 +0300

    throw

--- homework-g595-ulyanin/src/test/java/ru/mipt/java2016/homework/g595/ulyanin/task3/HighPerformanceStorageTest.java
+++ homework-g595-ulyanin/src/test/java/ru/mipt/java2016/homework/g595/ulyanin/task3/HighPerformanceStorageTest.java
@@ -1,7 +1,6 @@
 package ru.mipt.java2016.homework.g595.ulyanin.task3;
 
 import ru.mipt.java2016.homework.base.task2.KeyValueStorage;
-import ru.mipt.java2016.homework.base.task2.MalformedDataException;
 import ru.mipt.java2016.homework.g595.ulyanin.task2.*;
 import ru.mipt.java2016.homework.tests.task2.Student;
 import ru.mipt.java2016.homework.tests.task2.StudentKey;
@@ -19,7 +18,7 @@ import java.security.NoSuchAlgorithmException;
 public class HighPerformanceStorageTest extends KeyValueStoragePerformanceTest {
 
     @Override
-    protected KeyValueStorage<String, String> buildStringsStorage(String path) throws MalformedDataException {
+    protected KeyValueStorage<String, String> buildStringsStorage(String path) {
         HighPerformancePreservingKeyValueStorage<String, String> result = null;
         try {
             result = new HighPerformancePreservingKeyValueStorage<>(
@@ -34,7 +33,7 @@ public class HighPerformanceStorageTest extends KeyValueStoragePerformanceTest {
     }
 
     @Override
-    protected KeyValueStorage<Integer, Double> buildNumbersStorage(String path) throws MalformedDataException {
+    protected KeyValueStorage<Integer, Double> buildNumbersStorage(String path) {
         HighPerformancePreservingKeyValueStorage<Integer, Double> result = null;
         try {
             result = new HighPerformancePreservingKeyValueStorage<>(
@@ -49,7 +48,7 @@ public class HighPerformanceStorageTest extends KeyValueStoragePerformanceTest {
     }
 
     @Override
-    protected KeyValueStorage<StudentKey, Student> buildPojoStorage(String path) throws MalformedDataException {
+    protected KeyValueStorage<StudentKey, Student> buildPojoStorage(String path) {
         HighPerformancePreservingKeyValueStorage<StudentKey, Student> result = null;
         try {
             result = new HighPerformancePreservingKeyValueStorage<>(
