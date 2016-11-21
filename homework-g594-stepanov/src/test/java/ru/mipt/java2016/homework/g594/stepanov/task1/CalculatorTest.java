153f99b5acc09deb23899702c23a750e3c58052d
Ilia <irkstepanov@mail.ru>
2016-11-21 22:34:38 +0300

    Files renamed

--- homework-g594-stepanov/src/test/java/ru/mipt/java2016/homework/g594/stepanov/task1/CalculatorTest.java
+++ homework-g594-stepanov/src/test/java/ru/mipt/java2016/homework/g594/stepanov/task1/CalculatorTest.java
@@ -6,7 +6,7 @@ import ru.mipt.java2016.homework.tests.task1.AbstractCalculatorTest;
 /**
  * Created by ilia on 11.10.16.
  */
-public class CalculatorTestImplementation extends AbstractCalculatorTest {
+public class CalculatorTest extends AbstractCalculatorTest {
     @Override
     protected Calculator calc() {
         return new CalculatorImplementation();
--- homework-g594-stepanov/src/test/java/ru/mipt/java2016/homework/g594/stepanov/task3/NewStorageTest.java
+++ homework-g594-stepanov/src/test/java/ru/mipt/java2016/homework/g594/stepanov/task3/NewStorageTest.java
@@ -2,12 +2,11 @@ package ru.mipt.java2016.homework.g594.stepanov.task3;
 
 import ru.mipt.java2016.homework.base.task2.KeyValueStorage;
 import ru.mipt.java2016.homework.base.task2.MalformedDataException;
-import ru.mipt.java2016.homework.tests.task2.AbstractSingleFileStorageTest;
 import ru.mipt.java2016.homework.tests.task2.Student;
 import ru.mipt.java2016.homework.tests.task2.StudentKey;
 import ru.mipt.java2016.homework.tests.task3.KeyValueStoragePerformanceTest;
 
-public class NewStorageTestImplementation extends KeyValueStoragePerformanceTest {
+public class NewStorageTest extends KeyValueStoragePerformanceTest {
     @Override
     protected KeyValueStorage<String, String> buildStringsStorage(String path) throws MalformedDataException {
         return new NewStorageImplementation(path, "String", "String");
