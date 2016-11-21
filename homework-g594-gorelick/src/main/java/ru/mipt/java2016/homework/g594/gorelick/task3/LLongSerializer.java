343bf4df27066a4797aca4725d932772ef4b9c9b
Alexander Gorelick <gorelickalexander@gmail.com>
2016-11-22 00:22:46 +0300

    Final commit on task 3

--- homework-g594-gorelick/src/main/java/ru/mipt/java2016/homework/g594/gorelick/task3/LLongSerializer.java
+++
@@ -1,7 +0,0 @@
-package ru.mipt.java2016.homework.g594.gorelick.task3;
-
-/**
- * Created by alex on 11/21/16.
- */
-public class LLongSerializer {
-}
--- homework-g594-gorelick/src/test/java/ru/mipt/java2016/homework/g594/gorelick/task3/StudentKeySerializer.java
+++ homework-g594-gorelick/src/test/java/ru/mipt/java2016/homework/g594/gorelick/task3/StudentKeySerializer.java
@@ -1,32 +1,7 @@
-package ru.mipt.java2016.homework.g594.gorelick.task2;
-
-import ru.mipt.java2016.homework.tests.task2.StudentKey;
-
-import java.io.IOException;
-import java.nio.ByteBuffer;
+package ru.mipt.java2016.homework.g594.gorelick.task3;
 
 /**
- * Created by alex on 10/31/16.
+ * Created by alex on 11/21/16.
  */
-
-public class StudentKeySerializer implements Serializer<StudentKey> {
-    @Override
-    public ByteBuffer serialize(StudentKey object) throws IOException {
-        IntegerSerializer int_serialize = new IntegerSerializer();
-        StringSerializer str_serialize = new StringSerializer();
-        ByteBuffer group_id = int_serialize.serialize(object.getGroupId());
-        ByteBuffer name = str_serialize.serialize(object.getName());
-        ByteBuffer result = ByteBuffer.allocate(group_id.capacity() + name.capacity());
-        result.put(group_id.array());
-        result.put(name.array());
-        return result;
-    }
-    @Override
-    public StudentKey deserialize(ByteBuffer array) throws IOException {
-        IntegerSerializer int_deserialize = new IntegerSerializer();
-        StringSerializer str_deserialize = new StringSerializer();
-        Integer group_id = int_deserialize.deserialize(array);
-        String name = str_deserialize.deserialize(array);
-        return new StudentKey(group_id, name);
-    }
+public class StudentKeySerializer {
 }
