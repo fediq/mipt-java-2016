6831b5feb69945bfcf4248e4b1efb28fcba8915c
Vlad <27vl@mail.ru>
2016-11-25 23:12:36 +0300

    Update

--- homework-g597-mashurin/src/main/java/ru/mipt/java2016/homework/g597/mashurin/task3/Identification.java
+++ homework-g597-mashurin/src/main/java/ru/mipt/java2016/homework/g597/mashurin/task3/Identification.java
@@ -2,7 +2,7 @@ package ru.mipt.java2016.homework.g597.mashurin.task3;
 
 import java.io.RandomAccessFile;
 import java.io.IOException;
-interface Identification<Value> {
+public interface Identification<Value> {
     abstract Value read(RandomAccessFile input) throws IOException;
 
     abstract void write(RandomAccessFile output, Value value) throws IOException;
