610635e52b66a06759f2aa2100765bd1d3a506dd
aleksProsk <alexandriksasha@mail.ru>
2016-11-27 00:12:04 +0400

    Some mistakes were corrected

---
+++ homework-g595-proskurin/src/main/java/ru/mipt/java2016/homework/g595/proskurin/task3/DoubleSerializer.java
@@ -0,0 +1,16 @@
+package ru.mipt.java2016.homework.g595.proskurin.task3;
+
+import ru.mipt.java2016.homework.g595.proskurin.task3.MySerializer;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class DoubleSerializer implements MySerializer<Double> {
+    public void output(RandomAccessFile out, Double val) throws IOException {
+        out.writeDouble(val);
+    }
+
+    public Double input(RandomAccessFile in) throws IOException {
+        return in.readDouble();
+    }
+}
---
+++ homework-g595-proskurin/src/main/java/ru/mipt/java2016/homework/g595/proskurin/task3/StringSerializer.java
@@ -0,0 +1,16 @@
+package ru.mipt.java2016.homework.g595.proskurin.task3;
+
+import ru.mipt.java2016.homework.g595.proskurin.task3.MySerializer;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class StringSerializer implements MySerializer<String> {
+    public void output(RandomAccessFile out, String val) throws IOException {
+        out.writeUTF(val);
+    }
+
+    public String input(RandomAccessFile in) throws IOException {
+        return in.readUTF();
+    }
+}
