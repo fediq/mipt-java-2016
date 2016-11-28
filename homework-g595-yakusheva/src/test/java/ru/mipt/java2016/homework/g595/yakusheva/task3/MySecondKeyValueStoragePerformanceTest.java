8da9fe482763357ce2d501a508bbcd8c463505e8
Stager <stager108@mail.ru>
2016-11-28 23:41:08 +0300

    cache added

--- homework-g595-yakusheva/src/test/java/ru/mipt/java2016/homework/g595/yakusheva/task3/MySecondKeyValueStoragePerformanceTest.java
+++ homework-g595-yakusheva/src/test/java/ru/mipt/java2016/homework/g595/yakusheva/task3/MySecondKeyValueStoragePerformanceTest.java
@@ -14,17 +14,17 @@ import ru.mipt.java2016.homework.tests.task3.KeyValueStoragePerformanceTest;
 public class MySecondKeyValueStoragePerformanceTest extends KeyValueStoragePerformanceTest {
     @Override
     protected KeyValueStorage<String, String> buildStringsStorage(String path) throws MalformedDataException {
-        return new MySecondKeyValueStorage<String, String>(path, new MyStringSerializer(), new MyStringSerializer());
+        return new MySecondKeyValueStorage<String, String>(path, new MyStringSerializer(), new MyStringSerializer(), 0);
     }
 
     @Override
     protected KeyValueStorage<Integer, Double> buildNumbersStorage(String path) throws MalformedDataException {
-        return new MySecondKeyValueStorage<Integer, Double>(path, new MyIntegerSerializer(), new MyDoubleSerializer());
+        return new MySecondKeyValueStorage<Integer, Double>(path, new MyIntegerSerializer(), new MyDoubleSerializer(), 0);
     }
 
     @Override
     protected KeyValueStorage<StudentKey, Student> buildPojoStorage(String path) throws MalformedDataException {
-        return new MySecondKeyValueStorage<StudentKey, Student>(path, new MyStudentKeySerializer(), new MyStudentValueSerializer());
+        return new MySecondKeyValueStorage<StudentKey, Student>(path, new MyStudentKeySerializer(), new MyStudentValueSerializer(), 0);
     }
 
     /*@Test
