6bd32f77edcdb011a7bf59ce0e404674b44929ba
Alex <grishutin@179.ru>
2016-12-08 02:33:02 +0300

    g597.grishutin.task3 (#282)
    * seems to be implemented, but some awkward error with temp directory happens, seems to be Windows-local problem.
    
    * forgot to add main class lol
    
    * Fixed all tests except one with with multiple modifications. Merge remote-tracking branch 'remotes/upstream/master'
    
    # Conflicts:
    #	pom.xml
    
    * yeaah all test passed yeeah
    
    * task3: passing 12/16 tests, some issue with deleting file.
    
    * now passing all tests. Still need some optimizations
    
    * and... codestyle fixes, right
    
    * please pass
    
    * Now keys+offsets and values are stored separately. Gives some real speed boost.
    
    * renaming of this superlarge blablablaSerializationStrategy to blablablaSerializer. Also added guava cache and code is rewritten.
    
    * now passing tests on windows
    
    * now with deleting obsolete values
    
    * some cleanup and added thread-safety
    
    * now really deletes obsolete entries. Some code refactoring: unchecked exception to pass through interface methods, codestyle.
    
    * added space between try and {
    
    * https://pp.vk.me/c403118/v403118134/315d/nnpf7L0wcnQ.jpg
    
    * now really passing tests on windows
    
    * removed unused imports
    
    * a bit more flexible serialization strategies
    
    * removed unused imports
    
    * and even more
    
    * and even more code cleanup
    
    * and even more code cleanup
    
    * codestyle

--- homework-g597-grishutin/src/test/java/ru/mipt/java2016/homework/g597/grishutin/task3/LargeKeyValueStorageTest.java
+++ homework-g597-grishutin/src/test/java/ru/mipt/java2016/homework/g597/grishutin/task3/LargeKeyValueStorageTest.java
@@ -1,20 +1,22 @@
-package ru.mipt.java2016.homework.g597.grishutin.task2;
+package ru.mipt.java2016.homework.g597.grishutin.task3;
 
 import ru.mipt.java2016.homework.base.task2.KeyValueStorage;
-import ru.mipt.java2016.homework.tests.task2.AbstractSingleFileStorageTest;
+import ru.mipt.java2016.homework.g597.grishutin.task2.*;
 import ru.mipt.java2016.homework.tests.task2.Student;
 import ru.mipt.java2016.homework.tests.task2.StudentKey;
+import ru.mipt.java2016.homework.tests.task3.KeyValueStoragePerformanceTest;
 
 import java.io.IOException;
 
-public class GrishutinKeyValueStorageTest extends AbstractSingleFileStorageTest {
+
+public class LargeKeyValueStorageTest extends KeyValueStoragePerformanceTest {
     @Override
     protected KeyValueStorage<String, String> buildStringsStorage(String path) {
         try {
-            return new GrishutinKeyValueStorage<>(path,
+            return new LargeKeyValueStorage<>(path,
                     new StringSerializer(),
                     new StringSerializer());
-        } catch (IOException | IllegalAccessException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
@@ -23,10 +25,10 @@ public class GrishutinKeyValueStorageTest extends AbstractSingleFileStorageTest 
     @Override
     protected KeyValueStorage<Integer, Double> buildNumbersStorage(String path) {
         try {
-            return new GrishutinKeyValueStorage<>(path,
+            return new LargeKeyValueStorage<>(path,
                     new IntegerSerializer(),
                     new DoubleSerializer());
-        } catch (IOException | IllegalAccessException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
@@ -35,10 +37,10 @@ public class GrishutinKeyValueStorageTest extends AbstractSingleFileStorageTest 
     @Override
     protected KeyValueStorage<StudentKey, Student> buildPojoStorage(String path) {
         try {
-            return new GrishutinKeyValueStorage<>(path,
+            return new LargeKeyValueStorage<>(path,
                     new StudentKeySerializationStrategy(),
                     new StudentSerializationStrategy());
-        } catch (IOException | IllegalAccessException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
