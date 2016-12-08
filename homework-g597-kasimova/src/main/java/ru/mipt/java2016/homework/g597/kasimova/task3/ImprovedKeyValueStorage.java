b103f0803eeb5113223df988dd954cec1f81cd63
Kasimova Nadezhda <nadezhda_kasimova@mail.ru>
2016-12-09 02:07:57 +0300

    Adds multithreading.

--- homework-g597-kasimova/src/main/java/ru/mipt/java2016/homework/g597/kasimova/task3/ImprovedKeyValueStorage.java
+++ homework-g597-kasimova/src/main/java/ru/mipt/java2016/homework/g597/kasimova/task3/ImprovedKeyValueStorage.java
@@ -8,19 +8,23 @@ import java.nio.channels.Channels;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Created by Надежда on 21.11.2016.
  */
 
 public class ImprovedKeyValueStorage<K, V> implements KeyValueStorage<K, V> {
+    private static final int MAX_SIZE_OF_MAP_FOR_CLEANING = 100;
+    private static final int BUFFER_SIZE = 10000;
+    private static final int MAX_SIZE = 100000;
+
+    private ReentrantReadWriteLock generalLock = new ReentrantReadWriteLock();
     private MSerialization<K> keySerializer;
     private MSerialization<V> valueSerializer;
     private MSerialization<Long> shiftSerializer = MSerialization.LONG_SERIALIZER;
     private RandomAccessFile fileForKeys;
     private RandomAccessFile fileForValues;
-    private static final int BUFFER_SIZE = 10000;
-    private static final int MAX_SIZE = 100000;
     private final Map<K, V> map = new HashMap<K, V>();
     private final Map<K, Long> keysAndShiftTable = new HashMap<K, Long>();
     private boolean closed;
@@ -79,108 +83,143 @@ public class ImprovedKeyValueStorage<K, V> implements KeyValueStorage<K, V> {
     @Override
     public V read(K key) {
         isClosed();
-        if (!keysAndShiftTable.containsKey(key)) {
-            return null;
-        }
+        generalLock.writeLock().lock();
         try {
-            if (keysAndShiftTable.get(key) == -1) {
-                return map.get(key);
-            } else {
-                fileForValues.seek(keysAndShiftTable.get(key));
-                DataInputStream valuesDataInputStream = new DataInputStream(new BufferedInputStream(
-                        Channels.newInputStream(fileForValues.getChannel()), BUFFER_SIZE));
-                return valueSerializer.deserializeFromStream(valuesDataInputStream);
+            if (!keysAndShiftTable.containsKey(key)) {
+                return null;
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Error!");
+            try {
+                if (keysAndShiftTable.get(key) == -1) {
+                    return map.get(key);
+                } else {
+                    fileForValues.seek(keysAndShiftTable.get(key));
+                    DataInputStream valuesDataInputStream = new DataInputStream(new BufferedInputStream(
+                            Channels.newInputStream(fileForValues.getChannel()), BUFFER_SIZE));
+                    return valueSerializer.deserializeFromStream(valuesDataInputStream);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Error!");
+            }
+        } finally {
+            generalLock.writeLock().unlock();
         }
     }
 
     @Override
     public boolean exists(K key) {
         isClosed();
-        return keysAndShiftTable.containsKey(key);
+        generalLock.readLock().lock();
+        try {
+            return keysAndShiftTable.containsKey(key);
+        } finally {
+            generalLock.readLock().unlock();
+        }
     }
 
     @Override
     public void write(K key, V value) {
         isClosed();
-        map.put(key, value);
-        keysAndShiftTable.put(key, (long) -1);
-        maxShift++;
-        if (map.size() > BUFFER_SIZE / 10 - 1) {
-            try {
-                fileForValues.seek(fileForValues.length());
-                Long startPosition = fileForValues.getFilePointer();
-                DataOutputStream valuesDataOutputStream = new DataOutputStream(new BufferedOutputStream(
-                        Channels.newOutputStream(fileForValues.getChannel()), MAX_SIZE));
-                for (K entry : map.keySet()) {
-                    keysAndShiftTable.put(entry, startPosition + (long) valuesDataOutputStream.size());
-                    valueSerializer.serializeToStream(map.get(entry), valuesDataOutputStream);
+        generalLock.writeLock().lock();
+        try {
+            map.put(key, value);
+            keysAndShiftTable.put(key, (long) -1);
+            maxShift++;
+            if (map.size() > MAX_SIZE_OF_MAP_FOR_CLEANING) {
+                try {
+                    fileForValues.seek(fileForValues.length());
+                    Long startPosition = fileForValues.getFilePointer();
+                    DataOutputStream valuesDataOutputStream = new DataOutputStream(new BufferedOutputStream(
+                            Channels.newOutputStream(fileForValues.getChannel()), MAX_SIZE));
+                    for (K entry : map.keySet()) {
+                        keysAndShiftTable.put(entry, startPosition + (long) valuesDataOutputStream.size());
+                        valueSerializer.serializeToStream(map.get(entry), valuesDataOutputStream);
+                    }
+                    map.clear();
+                    valuesDataOutputStream.flush();
+                } catch (IOException e) {
+                    throw new RuntimeException("error!");
                 }
-                map.clear();
-                valuesDataOutputStream.flush();
-            } catch (IOException e) {
-                throw new RuntimeException("error!");
             }
-        }
-        if (maxShift > keysAndShiftTable.size() * 2) {
-            try {
-                File newFileForValues = new File(way + File.separator + "tempFileForValues");
-                if (!newFileForValues.exists()) {
-                    newFileForValues.createNewFile();
-                }
-                RandomAccessFile newValueFile = new RandomAccessFile(way + File.separator + "tempFileForValues", "rw");
-                DataOutputStream newValuesDataOutputStream = new DataOutputStream(new BufferedOutputStream(
-                        Channels.newOutputStream(newValueFile.getChannel()), MAX_SIZE));
-                newValueFile.writeInt(keysAndShiftTable.size());
-                Long startPosition = newValueFile.getFilePointer();
-                for (K entry : keysAndShiftTable.keySet()) {
-                    fileForValues.seek(keysAndShiftTable.get(entry));
-                    DataInputStream valuesDataInputStream = new DataInputStream(new BufferedInputStream(
-                            Channels.newInputStream(fileForValues.getChannel()), BUFFER_SIZE));
-                    keysAndShiftTable.put(entry, startPosition + (long) newValuesDataOutputStream.size());
-                    valueSerializer.serializeToStream(valueSerializer.deserializeFromStream(valuesDataInputStream),
-                            newValuesDataOutputStream);
-                }
-                newValuesDataOutputStream.flush();
-                newValueFile.close();
-                fileForValues.close();
-                if (!valuesFile.delete()) {
-                    throw new IOException("Error!");
-                }
-                if (!newFileForValues.renameTo(valuesFile)) {
-                    throw new IOException("Error!");
-                }
-                fileForValues = new RandomAccessFile(valuesFile, "rw");
+            if (maxShift > keysAndShiftTable.size() * 2) {
+                try {
+                    File newFileForValues = new File(way + File.separator + "tempFileForValues");
+                    if (!newFileForValues.exists()) {
+                        newFileForValues.createNewFile();
+                    }
+                    try (RandomAccessFile newValueFile = new RandomAccessFile(way + File.separator + "tempFileForValues", "rw");) {
+                        DataOutputStream newValuesDataOutputStream = new DataOutputStream(new BufferedOutputStream(
+                                Channels.newOutputStream(newValueFile.getChannel()), MAX_SIZE));
+                        newValueFile.writeInt(keysAndShiftTable.size());
+                        Long startPosition = newValueFile.getFilePointer();
+                        for (K entry : keysAndShiftTable.keySet()) {
+                            fileForValues.seek(keysAndShiftTable.get(entry));
+                            DataInputStream valuesDataInputStream = new DataInputStream(new BufferedInputStream(
+                                    Channels.newInputStream(fileForValues.getChannel()), BUFFER_SIZE));
+                            keysAndShiftTable.put(entry, startPosition + (long) newValuesDataOutputStream.size());
+                            valueSerializer.serializeToStream(valueSerializer.deserializeFromStream(valuesDataInputStream),
+                                    newValuesDataOutputStream);
+                        }
+                        newValuesDataOutputStream.flush();
+                        newValueFile.close();
+                        fileForValues.close();
 
-            } catch (IOException e) {
-                throw new RuntimeException("Error!");
+                        if (!valuesFile.delete()) {
+                            throw new IOException("Error!");
+                        }
+                        if (!newFileForValues.renameTo(valuesFile)) {
+                            throw new IOException("Error!");
+                        }
+                    }
+                    fileForValues = new RandomAccessFile(valuesFile, "rw");
+
+                } catch (IOException e) {
+                    throw new RuntimeException("Error!");
+                }
             }
+        } finally {
+            generalLock.writeLock().unlock();
         }
     }
 
     @Override
     public void delete(K key) {
         isClosed();
-        keysAndShiftTable.remove(key);
+        generalLock.writeLock().lock();
+        try {
+            keysAndShiftTable.remove(key);
+        } finally {
+            generalLock.writeLock().unlock();
+        }
     }
 
     @Override
     public Iterator<K> readKeys() {
         isClosed();
-        return keysAndShiftTable.keySet().iterator();
+        generalLock.readLock().lock();
+        try {
+            return keysAndShiftTable.keySet().iterator();
+        } finally {
+            generalLock.readLock().unlock();
+        }
     }
 
     @Override
     public int size() {
         isClosed();
-        return keysAndShiftTable.size();
+        generalLock.readLock().lock();
+        try {
+            return keysAndShiftTable.size();
+        } finally {
+            generalLock.readLock().unlock();
+        }
     }
 
     @Override
     public void close() throws IOException {
-        isClosed();
+        if (closed) {
+            return;
+        }
+        generalLock.writeLock().lock();
         try {
             fileForValues.seek(fileForValues.length());
             Long startPosition = fileForValues.getFilePointer();
@@ -192,22 +231,22 @@ public class ImprovedKeyValueStorage<K, V> implements KeyValueStorage<K, V> {
             }
             map.clear();
             valuesDataOutputStream.flush();
-        } catch (IOException e) {
-            throw new RuntimeException("Error!");
-        }
-        fileForKeys.seek(0);
-        DataOutputStream keysDataOutputStream = new DataOutputStream(new BufferedOutputStream(
-                Channels.newOutputStream(fileForKeys.getChannel()), MAX_SIZE));
-        fileForKeys.writeInt(keysAndShiftTable.size());
-        fileForKeys.writeInt(maxShift);
+            fileForKeys.seek(0);
+            DataOutputStream keysDataOutputStream = new DataOutputStream(new BufferedOutputStream(
+                    Channels.newOutputStream(fileForKeys.getChannel()), MAX_SIZE));
+            fileForKeys.writeInt(keysAndShiftTable.size());
+            fileForKeys.writeInt(maxShift);
 
-        for (K entry : keysAndShiftTable.keySet()) {
-            keySerializer.serializeToStream(entry, keysDataOutputStream);
-            shiftSerializer.serializeToStream(keysAndShiftTable.get(entry), keysDataOutputStream);
+            for (K entry : keysAndShiftTable.keySet()) {
+                keySerializer.serializeToStream(entry, keysDataOutputStream);
+                shiftSerializer.serializeToStream(keysAndShiftTable.get(entry), keysDataOutputStream);
+            }
+            keysDataOutputStream.flush();
+            fileForValues.close();
+            fileForKeys.close();
+            closed = true;
+        } finally {
+            generalLock.writeLock().unlock();
         }
-        keysDataOutputStream.flush();
-        fileForValues.close();
-        fileForKeys.close();
-        closed = true;
     }
 }
