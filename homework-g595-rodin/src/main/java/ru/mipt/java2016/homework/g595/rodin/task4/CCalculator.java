50d702dd50366ed2658ee3196d65199eb49f2738
Dmitry <kod744@gmail.com>
2016-12-10 12:32:30 +0300

    commit before update

---
+++ homework-g595-rodin/src/main/java/ru/mipt/java2016/homework/g595/rodin/task4/CCalculator.java
@@ -0,0 +1,7 @@
+package ru.mipt.java2016.homework.g595.rodin.task4;
+
+/**
+ * Created by dmitry on 26.11.16.
+ */
+public class CCalculator {
+}
---
+++ homework-g595-rodin/src/main/java/ru/mipt/java2016/homework/g595/rodin/task4/database/CDatabase.java
@@ -0,0 +1,7 @@
+package ru.mipt.java2016.homework.g595.rodin.task4.database;
+
+/**
+ * Created by dmitry on 26.11.16.
+ */
+public class CDatabase {
+}
