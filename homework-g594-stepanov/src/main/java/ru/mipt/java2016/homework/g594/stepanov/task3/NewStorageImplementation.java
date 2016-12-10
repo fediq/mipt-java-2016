2797337c163c1b550c921ff2d9e40a2a369830d8
Ilia <irkstepanov@mail.ru>
2016-12-10 22:40:10 +0300

    Fix: add hash and lock

--- homework-g594-stepanov/src/main/java/ru/mipt/java2016/homework/g594/stepanov/task3/NewStorageImplementation.java
+++ homework-g594-stepanov/src/main/java/ru/mipt/java2016/homework/g594/stepanov/task3/NewStorageImplementation.java
@@ -1,11 +1,12 @@
 package ru.mipt.java2016.homework.g594.stepanov.task3;
 
 import ru.mipt.java2016.homework.base.task2.KeyValueStorage;
+import ru.mipt.java2016.homework.base.task2.MalformedDataException;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.zip.Adler32;
 
 public class NewStorageImplementation<K, V> implements KeyValueStorage {
 
@@ -16,11 +17,26 @@ public class NewStorageImplementation<K, V> implements KeyValueStorage {
     private String temporaryStorage;
     private String keyType;
     private String valueType;
+    private File lockFile;
+    private File hashFile;
     private boolean closed = false;
+    private Adler32 adler;
 
-    public NewStorageImplementation(String directory, String keyType, String valueType) {
-        checkClosed();
+    public NewStorageImplementation(
+            String directory, String keyType, String valueType) throws MalformedDataException {
+        //checkClosed();
+        adler = new Adler32();
         this.directory = directory;
+        lockFile = new File(directory + "/Lock.txt");
+        hashFile = new File(directory + "/Hash.txt");
+        while (lockFile.exists()) {
+            throw new MalformedDataException("Storage busy!");
+        }
+        try {
+            lockFile.createNewFile();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         this.keyType = keyType;
         this.valueType = valueType;
         StringBuilder sb = new StringBuilder(directory);
@@ -37,6 +53,38 @@ public class NewStorageImplementation<K, V> implements KeyValueStorage {
             Long currentOffset = fileOperation.getCurrentInputOffset();
             Object value = fileOperation.readCurrentValue();
             keyOffsets.put(key, currentOffset);
+            adler.update(key.toString().getBytes());
+            adler.update(value.toString().getBytes());
+        }
+        if (!hashFile.exists()) {
+            try {
+                hashFile.createNewFile();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            PrintWriter outputStream = null;
+            try {
+                outputStream = new PrintWriter(new FileOutputStream(hashFile));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            int curr = (int) adler.getValue();
+            outputStream.print(new Integer(curr));
+            outputStream.close();
+        }
+        BufferedReader inputStream = null;
+        Integer hash = 0;
+        try {
+            inputStream = new BufferedReader(new FileReader(hashFile));
+            String s = inputStream.readLine();
+            hash = Integer.parseInt(s);
+            inputStream.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        int curr = (int) adler.getValue();
+        if (!hash.equals(new Integer(curr))) {
+            throw new MalformedDataException("Hash doesn't match");
         }
         fileOperation.setToStart();
     }
@@ -90,6 +138,7 @@ public class NewStorageImplementation<K, V> implements KeyValueStorage {
     @Override
     public void flush() {
         checkClosed();
+        adler = new Adler32();
         FileOperationFactory factory = new FileOperationFactory(temporaryStorage, keyType, valueType);
         FileOperation temporaryFileOperation = factory.getFileOperation();
         fileOperation.setToStart();
@@ -100,6 +149,8 @@ public class NewStorageImplementation<K, V> implements KeyValueStorage {
             if (keyOffsets.containsKey(key) && keyOffsets.get(key).equals(currentOffset)) {
                 temporaryFileOperation.appendKey(key);
                 temporaryFileOperation.appendValue(value);
+                adler.update(key.toString().getBytes());
+                adler.update(value.toString().getBytes());
             }
         }
         temporaryFileOperation.close();
@@ -110,6 +161,20 @@ public class NewStorageImplementation<K, V> implements KeyValueStorage {
         temporary.renameTo(new File(mainStorage));
         factory = new FileOperationFactory(mainStorage, keyType, valueType);
         fileOperation = factory.getFileOperation();
+        hashFile.delete();
+        try {
+            hashFile.createNewFile();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        PrintWriter outputStream = null;
+        try {
+            outputStream = new PrintWriter(new FileOutputStream(hashFile, true));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        outputStream.print((int) adler.getValue());
+        outputStream.close();
     }
 
     @Override
@@ -117,6 +182,7 @@ public class NewStorageImplementation<K, V> implements KeyValueStorage {
         checkClosed();
         flush();
         closed = true;
+        lockFile.delete();
     }
 
     void checkClosed() {
