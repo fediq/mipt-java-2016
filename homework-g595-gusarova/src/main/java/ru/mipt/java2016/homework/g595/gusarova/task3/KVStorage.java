16fa1204c30fc10924e25a96088579008d0dd41f
DariaGusarova <daria.gusarova7@yandex.ru>
2016-12-15 01:24:47 +0300

    Add flush, solve the problem of thread safety and data validation

--- homework-g595-gusarova/src/main/java/ru/mipt/java2016/homework/g595/gusarova/task3/KVStorage.java
+++ homework-g595-gusarova/src/main/java/ru/mipt/java2016/homework/g595/gusarova/task3/KVStorage.java
@@ -4,6 +4,7 @@ import ru.mipt.java2016.homework.base.task2.KeyValueStorage;
 import ru.mipt.java2016.homework.base.task2.MalformedDataException;
 
 import java.io.*;
+import java.nio.channels.FileLock;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -14,22 +15,63 @@ import java.util.Iterator;
 public class KVStorage<K, V> implements KeyValueStorage<K, V> {
     private RandomAccessFile fileWithKeysAndOffsets;
     private RandomAccessFile fileWithValues;
+    private RandomAccessFile temperary;
+    private final FileLock lockFile;
+    private final RandomAccessFile fileForLock;
+    static final Object objectForLock = new Object();
+
+
     private SerializerAndDeserializer<K> serializerAndDeserializerForKey;
     private SerializerAndDeserializer<V> serializerAndDeserializerForValue;
     private SerializerAndDeserializer<Long> serializerAndDeserializerForOffsets;
     private HashMap<K, Long> map;
     private Boolean baseClosed = false;
     private Long maxOffset;
+    private Integer counter = 0;
+    private String validationString = "data is correct";
 
     private void addKeysAndOffsets() throws IOException {
         map = new HashMap<K, Long>();
         DataInput input = fileWithKeysAndOffsets;
-        fileWithKeysAndOffsets.seek(0);
         int size = input.readInt();
         maxOffset = input.readLong();
+        counter = input.readInt();
         for (int i = 0; i < size; i++) {
             map.put(serializerAndDeserializerForKey.deserialize(input),
                     serializerAndDeserializerForOffsets.deserialize(input));
+        }
+    }
+
+    public void validation() throws IOException {
+        Integer coun = 0;
+        if (fileWithKeysAndOffsets.length() == 0) {
+            coun++;
+        } else {
+            DataInput input = fileWithKeysAndOffsets;
+            fileWithKeysAndOffsets.seek(0);
+            String str = input.readUTF();
+            if (str.equals(validationString)) {
+                coun++;
+            }
+        }
+        if (fileWithValues.length() == 0) {
+            coun++;
+            DataOutput output = fileWithValues;
+            fileWithValues.seek(0);
+            output.writeUTF(validationString);
+            maxOffset = fileWithValues.getFilePointer() + 1;
+        } else {
+            DataInput input = fileWithValues;
+            fileWithValues.seek(0);
+            String str = input.readUTF();
+            if (str.equals(validationString)) {
+                coun++;
+            }
+        }
+        if (coun == 2) {
+            return;
+        } else {
+            throw new IOException();
         }
     }
 
@@ -41,10 +83,21 @@ public class KVStorage<K, V> implements KeyValueStorage<K, V> {
             throw new MalformedDataException("this path is incorrect");
         }
         try {
+            synchronized (objectForLock) {
+                fileForLock = new RandomAccessFile(path + File.separator + "Lock.txt", "rw");
+                lockFile = fileForLock.getChannel().lock();
+            }
+
             fileWithKeysAndOffsets = new RandomAccessFile(path + File.separator + "KeysAndOffsets.txt", "rw");
             fileWithValues = new RandomAccessFile(path + File.separator + "Values.txt", "rw");
+            temperary = new RandomAccessFile(path + File.separator + "Temperary.txt", "rw");
             fileWithValues.seek(0);
             maxOffset = fileWithValues.getFilePointer();
+            try {
+                validation();
+            } catch (IOException exp) {
+                throw new MalformedDataException("data was uncorrectly changed");
+            }
         } catch (IOException e) {
             throw new MalformedDataException("problems with files");
         }
@@ -56,6 +109,7 @@ public class KVStorage<K, V> implements KeyValueStorage<K, V> {
         } catch (IOException exp) {
             //base was never printed on disk
         }
+
     }
 
     @Override
@@ -95,6 +149,9 @@ public class KVStorage<K, V> implements KeyValueStorage<K, V> {
             fileWithValues.seek(maxOffset);
             DataOutput output = fileWithValues;
             serializerAndDeserializerForValue.serialize(value, output);
+            if (map.containsKey(key)) {
+                flush();
+            }
             map.put(key, maxOffset);
             maxOffset = fileWithValues.getFilePointer() + 1;
         } catch (IOException e) {
@@ -109,6 +166,7 @@ public class KVStorage<K, V> implements KeyValueStorage<K, V> {
         }
         if (map.containsKey(key)) {
             map.remove(key);
+            flush();
         }
     }
 
@@ -128,7 +186,37 @@ public class KVStorage<K, V> implements KeyValueStorage<K, V> {
         return map.size();
     }
 
-    public void flush() { }
+
+    @Override
+    public void flush() {
+        counter++;
+        if (counter > 3 * map.size()) {
+            try {
+                counter = 0;
+                DataOutput output = temperary;
+                DataInput input = fileWithValues;
+                temperary.seek(0);
+                for (K entry : map.keySet()) {
+                    fileWithValues.seek(map.get(entry));
+                    V temp = serializerAndDeserializerForValue.deserialize(input);
+                    map.replace(entry, temperary.getFilePointer());
+                    serializerAndDeserializerForValue.serialize(temp, output);
+                }
+                DataOutput secondOutput = fileWithValues;
+                DataInput secondInput = temperary;
+                fileWithValues.seek(0);
+                secondOutput.writeUTF(validationString);
+                for (K entry : map.keySet()) {
+                    temperary.seek(map.get(entry));
+                    V temp = serializerAndDeserializerForValue.deserialize(secondInput);
+                    map.replace(entry, fileWithValues.getFilePointer());
+                    serializerAndDeserializerForValue.serialize(temp, secondOutput);
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 
     @Override
     public void close() throws IOException {
@@ -136,17 +224,23 @@ public class KVStorage<K, V> implements KeyValueStorage<K, V> {
             throw new RuntimeException("base closed");
         }
         try {
+            temperary.close();
             fileWithValues.close();
             DataOutput output = fileWithKeysAndOffsets;
             fileWithKeysAndOffsets.seek(0);
+            output.writeUTF(validationString);
             output.writeInt(map.size());
             output.writeLong(maxOffset);
+            output.writeInt(counter);
             for (K entry : map.keySet()) {
                 serializerAndDeserializerForKey.serialize(entry, output);
                 serializerAndDeserializerForOffsets.serialize(map.get(entry), output);
             }
-            baseClosed = true;
+
             fileWithKeysAndOffsets.close();
+            baseClosed = true;
+            lockFile.release();
+            fileForLock.close();
         } catch (IOException exp) {
             System.out.println(exp.getMessage());
         }
--- homework-g595-gusarova/src/test/java/ru/mipt/java2016/homework/g595/gusarova/task3/KVStorageTest.java
+++ homework-g595-gusarova/src/test/java/ru/mipt/java2016/homework/g595/gusarova/task3/KVStorageTest.java
@@ -48,9 +48,4 @@ public class KVStorageTest extends KeyValueStoragePerformanceTest {
         }
         return temp;
     }
-
-
-
-
-
 }
