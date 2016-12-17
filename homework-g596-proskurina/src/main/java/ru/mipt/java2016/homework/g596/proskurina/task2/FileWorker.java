fd85dc1c1326d5baba95228f6088f74a07d61dc3
Ksenia Proskurina <ksushkapr@yandex.ru>
2016-12-17 21:58:03 +0300

    updatind

--- homework-g596-proskurina/src/main/java/ru/mipt/java2016/homework/g596/proskurina/task2/FileWorker.java
+++ homework-g596-proskurina/src/main/java/ru/mipt/java2016/homework/g596/proskurina/task2/FileWorker.java
@@ -1,50 +1,165 @@
 package ru.mipt.java2016.homework.g596.proskurina.task2;
 
 import java.io.*;
+import java.nio.ByteBuffer;
 
 /**
  * Created by Lenovo on 31.10.2016.
  */
-public class FileWorker {
+public class FileWorker implements Closeable{
 
-    private static void exists(String fileName) throws FileNotFoundException {
-        File file = new File(fileName);
-        if (!file.exists()) {
-            throw new FileNotFoundException(file.getName());
+    private final File file;
+    private final String fileName;
+
+    private InputStream readBuffer = null;
+    private OutputStream writeBuffer = null;
+
+    private long currentPositionInStream = 0;
+
+    public FileWorker(String fileName) {
+        this.file = new File(fileName);
+        this.fileName = fileName;
+    }
+
+    public void createFile() {
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("file didn't created");
+            throw new RuntimeException(e);
         }
     }
 
-    public static String read(String fileName) throws FileNotFoundException {
+    public void rename(String newName) {
+        File newFile = new File(newName);
+        file.renameTo(newFile);
+    }
 
-        StringBuffer inputData = new StringBuffer();
-        exists(fileName);
-        File file = new File(fileName);
-
-        try (BufferedReader in = new BufferedReader(new FileReader(file.getAbsoluteFile()))) {
-            String s;
-            while ((s = in.readLine()) != null) {
-                inputData.append(s);
-                inputData.append("\n");
-            }
-
+    public void appendMode() {  
+        try {
+            close();
+            writeBuffer = new FileOutputStream(file.getAbsoluteFile(), true);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return inputData.toString();
     }
 
-    public static void write(String fileName, String text) {
+    public boolean exist() {
+        return file.exists();
+    }
 
-        File file = new File(fileName);
-        try {
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            PrintWriter out = new PrintWriter(file.getAbsoluteFile());
+    private boolean innerExist() throws FileNotFoundException {
+        if (!exist()) {
+            throw new FileNotFoundException(fileName);
+        }
+        return true;
+    }
+
+    public void writeSubmit() {
+        if (writeBuffer != null) {
             try {
-                out.print(text);
-            } finally {
-                out.close();
+                writeBuffer.flush();
+                writeBuffer.close();
+                writeBuffer = null;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public long write(String str) {
+        try {
+            innerExist();
+            if (writeBuffer == null) {
+                writeBuffer = new BufferedOutputStream(new FileOutputStream(file.getAbsoluteFile()));
+            }
+            byte[] bytes = str.getBytes();
+            byte[] bytesNumber = ByteBuffer.allocate(4).putInt(bytes.length).array();
+            writeBuffer.write(bytesNumber);
+            writeBuffer.write(bytes);
+            return 4 + bytes.length;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String read() {
+        try {
+            innerExist();
+            if (readBuffer == null) {
+                readBuffer = new BufferedInputStream(new FileInputStream(file.getAbsoluteFile()));
+                currentPositionInStream = 0;
+            }
+            if (readBuffer.available() < 4) {
+                readBuffer.close();
+                readBuffer = null;
+                return null;
+            }
+            byte[] bytesNumberArray = new byte[4];
+            int bytesNumberArraySize = readBuffer.read(bytesNumberArray, 0, 4);
+            //addToCalc(bytes);
+            if (bytesNumberArraySize < 4) {
+                readBuffer.close();
+                throw new RuntimeException("Error in reading bytes number");
+            }
+            currentPositionInStream += bytesNumberArraySize;
+            int bytesNumber = ByteBuffer.wrap(bytesNumberArray).getInt();
+            byte[] bytesArray = new byte[bytesNumber];
+            int bytesArraySize = readBuffer.read(bytesArray, 0, bytesNumber);
+            //addToCalc(bytes);
+            if (bytesArraySize < bytesNumber) {
+                readBuffer.close();
+                throw new RuntimeException("Error in reading bytes");
+            }
+            currentPositionInStream += bytesArraySize;
+            return new String(bytesArray);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public long fileLength() {
+        try (FileInputStream inputStream = new FileInputStream(file.getAbsoluteFile())) {
+            return inputStream.available();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void goToPosition(long position) {
+        try {
+            innerExist();
+            if (readBuffer == null || currentPositionInStream > position) {
+                if (readBuffer != null) {
+                    readBuffer.close();
+                }
+                readBuffer = new BufferedInputStream(new FileInputStream(file.getAbsoluteFile()));
+                currentPositionInStream = 0;
+            }
+            while (currentPositionInStream < position) {
+                currentPositionInStream += readBuffer.skip(position - currentPositionInStream);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete() {
+        file.delete();
+    }
+
+    @Override
+    public void close() {
+        try {
+            if (readBuffer != null) {
+                readBuffer.close();
+                currentPositionInStream = 0;
+                readBuffer = null;
+            }
+            if (writeBuffer != null) {
+                writeBuffer.flush();
+                writeBuffer.close();
+                writeBuffer = null;
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
--- homework-g596-proskurina/src/main/java/ru/mipt/java2016/homework/g596/proskurina/task2/ImplementationKeyValueStorage.java
+++ homework-g596-proskurina/src/main/java/ru/mipt/java2016/homework/g596/proskurina/task2/ImplementationKeyValueStorage.java
@@ -1,65 +1,118 @@
 package ru.mipt.java2016.homework.g596.proskurina.task2;
 
-import ru.mipt.java2016.homework.base.task2.KeyValueStorage;
-
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Lenovo on 31.10.2016.
  */
-public class ImplementationKeyValueStorage<K, V> implements KeyValueStorage<K, V> {
+public class ImplementationKeyValueStorage<K, V> implements ru.mipt.java2016.homework.base.task2.KeyValueStorage<K, V> {
 
-    private final Map<K, V> map = new HashMap<>();
+    private final Map<K, Long> keyPositionMap = new HashMap<>();
 
     private final SerialiserInterface<K> keySerialiser;
     private final SerialiserInterface<V> valueSerialiser;
 
-    private final FileWorker file;
-    private final String fileName;
+    private final Set<K> deleteKeySet = new HashSet<>();
+
+    private final FileWorker keyPositionFile;
+    private final FileWorker valuesFile;
+    private final FileWorker deleteKeyFile;
+    private final FileWorker lockFile;
+    private final FileWorker validationFile;
+
+    private final String dirPath;
+
+    private Long currentPositionInValuesFile = new Long(0);
+    private final Integer lock = 42;
+
+    private boolean writing = true;
+    private boolean needRebuild = false;
     private static final String VALIDATION_STRING = "EtotFileZapisanMoeiProgoi";
     private boolean openFlag = true;
 
+    private LoadingCache<K, V> cacheValues = CacheBuilder.newBuilder()
+            .maximumSize(10)
+            .build(
+                    new CacheLoader<K, V>() {
+                        @Override
+                        public V load(K k) throws StorageException {
+                            V result = loadKey(keyPositionMap.get(k));
+                            if (result == null) {
+                                throw new StorageException("no such key");
+                            }
+                            return result;
+                        }
+                    });
 
-    public ImplementationKeyValueStorage(String keyName, String valueName,
+    public ImplementationKeyValueStorage(//String keyName, String valueName,
                                          SerialiserInterface<K> keySerialiser, SerialiserInterface<V> valueSerialiser,
                                          String directoryPath) {
 
         this.keySerialiser = keySerialiser;
         this.valueSerialiser = valueSerialiser;
 
-        file = new FileWorker();
-        fileName = directoryPath + File.separator + "myFile.db";
+        keyPositionMap = Collections.synchronizedMap(new HashMap<>());
 
-        try {
-            String inputData = file.read(fileName);
-
-            String[] tokens = inputData.split("\n");
-            if (!tokens[0].equals(VALIDATION_STRING)) {
-                throw new RuntimeException("Not my file");
-            }
-            if (!tokens[1].equals(keyName)) {
-                throw new RuntimeException("Wrong key type");
-            }
-            if (!tokens[2].equals(valueName)) {
-                throw new RuntimeException("Wrong value type");
-            }
-
-            Integer objectsNumber = Integer.parseInt(tokens[3]);
-            for (int i = 0; i < objectsNumber; ++i) {
-                K key = keySerialiser.deserialise(tokens[2 * i + 4]);
-                V value = valueSerialiser.deserialise(tokens[2 * i + 5]);
-                map.put(key, value);
-            }
-
-        } catch (FileNotFoundException e) {
-            writeData();
+        if (dirPath == null || dirPath.equals("")) {
+            this.dirPath = "";
+        } else {
+            this.dirPath = dirPath + File.separator;
         }
-
-
+        keyPositionFile = new FileWorker(this.dirPath + "keyPositionFile.db");
+        valuesFile = new FileWorker(this.dirPath + "valuesFile.db");
+        deleteKeyFile = new FileWorker(this.dirPath + "deleteKeyFile.db");
+        validationFile = new FileWorker(this.dirPath + "validationFile.db");
+        lockFile = new FileWorker(this.dirPath + "lockFile.db");
+        if (lockFile.exist()) {
+            throw new RuntimeException("there is working storage");
+        } else {
+            lockFile.createFile();
+        }
+        if (!keyPositionFile.exist()) {
+            keyPositionFile.createFile();
+            valuesFile.createFile();
+            deleteKeyFile.createFile();
+            validationFile.createFile();
+        } else {
+            currentPositionInValuesFile =  valuesFile.fileLength();
+            initStorage();
+        }
+         valuesFile.appendMode();
+    }
+    private void initStorage() {
+        keyPositionFile.close();
+        int cnt = 0;
+        String nextKey = keyPositionFile.read();
+        while (nextKey != null) {
+            Long position = Long.parseLong(keyPositionFile.read());
+            try {
+                keyPositionMap.put(keySerialiser.deserialise(nextKey), position);
+            } catch (StorageException e) {
+                throw new RuntimeException(e);
+            }
+            nextKey = keyPositionFile.read();
+            cnt++;
+        }
+        nextKey = deleteKeyFile.read();
+        while (nextKey != null) {
+            try {
+                K key = keySerialiser.deserialise(nextKey);
+                deleteKeySet.add(key);
+                keyPositionMap.remove(key);
+            } catch (StorageException e) {
+                throw new RuntimeException(e);
+            }
+            nextKey = deleteKeyFile.read();
+            cnt++;
+        }
+        keyPositionFile.appendMode();
+        deleteKeyFile.close();
+        if (cnt > 2 * keyPositionMap.size()) {
+            needRebuild = true;
+        }
     }
 
     private void checkIfFileIsOpen() {
@@ -70,61 +123,146 @@ public class ImplementationKeyValueStorage<K, V> implements KeyValueStorage<K, V
 
     @Override
     public V read(K key) {
-        checkIfFileIsOpen();
-        return map.get(key);
-
+        synchronized (lock) {
+            checkIfFileIsOpen();
+            Long offset = keyPositionMap.get(key);
+            if (offset != null) {
+                try {
+                    return cacheValues.get(key);
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                return null;
+            }
+        }
     }
 
     @Override
     public boolean exists(K key) {
-        checkIfFileIsOpen();
-        return map.containsKey(key);
+        synchronized (lock) {
+            checkIfFileIsOpen();
+            return keyPositionMap.containsKey(key);
+        }
     }
 
     @Override
     public void write(K key, V value) {
-        checkIfFileIsOpen();
-        map.put(key, value);
+        synchronized (lock) {
+            checkIfFileIsOpen();
+            deleteKeySet.remove(key);
+            keyPositionMap.put(key, currentPositionInValuesFile);
+            flush(key, value);
+        }
     }
 
     @Override
     public void delete(K key) {
-        checkIfFileIsOpen();
-        map.remove(key);
+        synchronized (lock) {
+            checkIfFileIsOpen();
+            deleteKeySet.add(key);
+            keyPositionMap.remove(key);
+        }
     }
 
     @Override
     public Iterator<K> readKeys() {
-        checkIfFileIsOpen();
-        return map.keySet().iterator();
+        synchronized (lock) {
+            checkIfFileIsOpen();
+            return keyPositionMap.keySet().iterator();
+        }
     }
-
     @Override
     public int size() {
-        checkIfFileIsOpen();
-        return map.size();
+        synchronized (lock) {
+            checkIfFileIsOpen();
+            return keyPositionMap.size();
+        }
     }
 
     @Override
-    public void close() {
-        checkIfFileIsOpen();
-        openFlag = false;
-        writeData();
-        map.clear();
-    }
-
-    public void writeData() {
-        StringBuffer text = new StringBuffer(VALIDATION_STRING + "\n");
-        text.append(keySerialiser.getType()).append('\n').append(valueSerialiser.getType()).append('\n');
-        text.append(map.size()).append('\n');
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            text.append(keySerialiser.serialise(entry.getKey()))
-                    .append('\n')
-                    .append(valueSerialiser.serialise(entry.getValue()))
-                    .append('\n');
+    public void close()  throws IOException {
+        synchronized (lock) {
+            if(openFlag) {
+                openFlag = false;
+                if (needRebuild) {
+                    rebuild();
+                } else {
+                    flushDeletes();
+                    deleteKeyFile.close();
+                    keyPositionFile.close();
+                     valuesFile.close();
+                    writeChecksum();
+                }
+                lockFile.delete();
+            }
         }
-        file.write(fileName, text.toString());
-
     }
 
+    private void rebuild() {
+        FileWorker newVal = new FileWorker(dirPath + "nva.db", false);
+        newVal.createFile();
+        deleteKeyFile.close();
+        deleteKeyFile.delete();
+        deleteKeyFile.createFile();
+        validationFile.close();
+        validationFile.delete();
+        validationFile.createFile();
+        keyPositionFile.close();
+        keyPositionFile.delete();
+        keyPositionFile.createFile();
+        currentPositionInValuesFile = 0L;
+        for (Map.Entry<K, Long> entry: keyPositionMap.entrySet()) {
+            keyPositionFile.write(keySerialiser.serialise(entry.getKey()));
+            keyPositionFile.write(currentPositionInValuesFile.toString());
+            currentPositionInValuesFile += newVal.write(valueSerialiser.serialise(loadKey(entry.getValue())));
+        }
+        keyPositionFile.writeSubmit();
+        newVal.writeSubmit();
+        writeChecksum();
+         valuesFile.close();
+         valuesFile.delete();
+        newVal.rename(dirPath + "valuesFile.db");
+        newVal.close();
+    }
+
+    private void writeChecksum() {
+        validationFile.close();
+        validationFile.write(Long.toString(keyPositionFile.getCheckSum()));
+        validationFile.writeSubmit();
+    }
+    private V loadKey(long position) {
+        if (writing) {
+             valuesFile.close();
+            writing = false;
+        }
+         valuesFile.goToPosition(position);
+        try {
+            return valueSerialiser.deserialise( valuesFile.read());
+        } catch (StorageException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void flush(K key, V value) {
+        if (!writing) {
+             valuesFile.close();
+             valuesFile.appendMode();
+            writing = true;
+        }
+        keyPositionFile.write(keySerialiser.serialise(key));
+        keyPositionFile.write(currentPositionInValuesFile.toString());
+        currentPositionInValuesFile +=  valuesFile.write(valueSerialiser.serialise(value));
+    }
+
+    private void flushDeletes() {
+        if (!deleteKeyFile.exist()) {
+            deleteKeyFile.createFile();
+        }
+        for (K entry : deleteKeySet) {
+            deleteKeyFile.write(keySerialiser.serialise(entry));
+        }
+        deleteKeyFile.writeSubmit();
+        deleteKeySet.clear();
+    }
 }
