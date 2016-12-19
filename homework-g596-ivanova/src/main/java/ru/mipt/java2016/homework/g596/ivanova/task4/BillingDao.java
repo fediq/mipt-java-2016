007b5da40364dbfd67ca6ce8af68ac4fb89681d3
Julia Ivanova <ivanova-j-ulia@yandex.ru>
2016-12-20 00:20:27 +0300

    Add task 4

---
+++ homework-g596-ivanova/src/main/java/ru/mipt/java2016/homework/g596/ivanova/task4/BillingDao.java
@@ -0,0 +1,73 @@
+package ru.mipt.java2016.homework.g596.ivanova.task4;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.mipt.java2016.homework.base.task1.ParsingException;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+@Repository
+public class BillingDao {
+    private static final Logger LOG = LoggerFactory.getLogger(BillingDao.class);
+
+    @Autowired
+    private DataSource dataSource;
+
+    private JdbcTemplate jdbcTemplate;
+
+    @PostConstruct
+    public void postConstruct() {
+        jdbcTemplate = new JdbcTemplate(dataSource, false);
+        initSchema();
+    }
+
+    public void initSchema() {
+        LOG.trace("Initializing schema");
+        jdbcTemplate.execute("CREATE SCHEMA IF NOT EXISTS billing");
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS billing.users " +
+                "(username VARCHAR PRIMARY KEY, password VARCHAR, enabled BOOLEAN)");
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS billing.variables " +
+                "(username VARCHAR, name VARCHAR, value DOUBLE)");
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS billing.functions " +
+                "(username VARCHAR, name VARCHAR, arguments VARCHAR, expression VARCHAR)");
+        addUserIfNotExists("username", "password", true);
+    }
+
+    boolean addUserIfNotExists(String username, String password, boolean enabled) {
+        try {
+            loadUser(username);
+            return false;
+        } catch (EmptyResultDataAccessException e) {
+            jdbcTemplate.update("INSERT INTO billing.users VALUES (?, ?, ?)",
+                    new Object[]{username, password, enabled});
+            return true;
+        }
+    }
+
+    public BillingUser loadUser(String username) throws EmptyResultDataAccessException {
+        LOG.trace("Querying for user " + username);
+        return jdbcTemplate.queryForObject(
+                "SELECT username, password, enabled FROM billing.users WHERE username = ?",
+                new Object[]{username},
+                new RowMapper<BillingUser>() {
+                    @Override
+                    public BillingUser mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        return new BillingUser(
+                                rs.getString("username"),
+                                rs.getString("password"),
+                                rs.getBoolean("enabled")
+                        );
+                    }
+                }
+        );
+    }
+}
---
+++ homework-g596-ivanova/src/main/java/ru/mipt/java2016/homework/g596/ivanova/task4/RestCalculatorController.java
@@ -0,0 +1,26 @@
+package ru.mipt.java2016.homework.g596.ivanova.task4;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import ru.mipt.java2016.homework.base.task1.ParsingException;
+import ru.mipt.java2016.homework.g596.ivanova.task1.BestCalculatorEver;
+
+@RestController
+public class RestCalculatorController {
+    @Autowired
+    private BestCalculatorEver calculator;
+
+    @Autowired
+    private BillingDao billingDao;
+
+    @RequestMapping(path = "/calculate", method = RequestMethod.POST,
+            consumes = "text/plain", produces = "text/plain")
+    public String calculate(Authentication authentication, @RequestBody String expression) throws
+            ParsingException {
+        return calculator.calculate(expression) + "\n";
+    }
+}
