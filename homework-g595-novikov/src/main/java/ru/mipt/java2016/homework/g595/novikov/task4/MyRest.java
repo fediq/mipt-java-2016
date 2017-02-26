f5ee6334f3ad93b7fda1bcf7192d8b69e6541ff7
reccy168 <nie1997@yandex.ru>
2017-02-26 23:11:32 +0300

    test commit

--- homework-g595-novikov/src/main/java/ru/mipt/java2016/homework/g595/novikov/task4/MyRest.java
+++ homework-g595-novikov/src/main/java/ru/mipt/java2016/homework/g595/novikov/task4/MyRest.java
@@ -1,17 +1,21 @@
 package ru.mipt.java2016.homework.g595.novikov.task4;
 
-import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.mipt.java2016.homework.base.task1.Calculator;
 import ru.mipt.java2016.homework.base.task1.ParsingException;
 
 import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
@@ -23,21 +27,34 @@ import static org.springframework.web.bind.annotation.RequestMethod.PUT;
  * Created by igor on 11/26/16.
  */
 @RestController
-public class MyREST {
-    private CalculatorWithMethods calculator = new CalculatorState();
+class MyREST {
+    // spring authentication "how to receive User object" manual
+    // https://www.mkyong.com/spring-security/get-current-logged-in-username-in-spring-security/
+    private static final Logger LOG = LoggerFactory.getLogger(MyREST.class);
+
+    @Autowired UserDao userDao;
 
     @RequestMapping(value = "variable", method = GET, consumes = "text/plain", produces = "text/plain")
     @ResponseBody
     public String listVariables() {
+        CalculatorState calculator =
+                ((MyUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+                        .getCalculator();
+        LOG.trace("get variables list request");
         return calculator.getVariablesList().toString();
     }
 
     @RequestMapping(value = "variable/{name}", method = GET, consumes = "text/plain", produces = "text/plain")
     @ResponseBody
     public ResponseEntity<String> getVariable(@PathVariable(value = "name") String name) {
+        CalculatorState calculator =
+                ((MyUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+                        .getCalculator();
+        LOG.trace("get variable " + name + " request");
         Double result = calculator.getVariable(name);
         if (result == null) {
-            return new ResponseEntity<String>("variable not found : " + name, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<String>("variable not found : " + name,
+                    HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<String>(Double.toString(result), HttpStatus.OK);
     }
@@ -46,17 +63,23 @@ public class MyREST {
     @ResponseBody
     public ResponseEntity<String> setVariable(@PathVariable(value = "name") String name,
             @RequestBody String value) {
-        try {
-            calculator.addVariable(name, Double.parseDouble(value));
-        } catch (ParsingException e) {
-            return new ResponseEntity<String>("parsing error", HttpStatus.BAD_REQUEST);
-        }
+        System.err.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        CalculatorState calculator =
+                ((MyUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+                        .getCalculator();
+        LOG.trace("set variable " + name + " to " + value + " request");
+        calculator.addVariable(name, Double.parseDouble(value));
+        updateUser(calculator);
         return new ResponseEntity<String>("", HttpStatus.OK);
     }
 
     @RequestMapping(value = "variable/{name}", method = DELETE, consumes = "text/plain", produces = "text/plain")
     @ResponseBody
     public String deleteVariable(@PathVariable String name) {
+        CalculatorState calculator =
+                ((MyUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+                        .getCalculator();
+        LOG.trace("remove variable " + name + " request");
         calculator.deleteVariable(name);
         return "";
     }
@@ -64,12 +87,20 @@ public class MyREST {
     @RequestMapping(value = "/function", method = GET, consumes = "text/plain", produces = "text/plain")
     @ResponseBody
     public String functionsList() {
+        CalculatorState calculator =
+                ((MyUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+                        .getCalculator();
+        LOG.trace("get functions list request");
         return calculator.getFunctionsList().toString(); // ????
     }
 
     @RequestMapping(value = "/function/{name}", method = GET, consumes = "text/plain", produces = "text/plain")
     @ResponseBody
     public String getFunction(@PathVariable(value = "name") String name) {
+        CalculatorState calculator =
+                ((MyUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+                        .getCalculator();
+        LOG.trace("get function " + name + " request");
         return calculator.getFunction(name).toString();
     }
 
@@ -77,17 +108,26 @@ public class MyREST {
     @ResponseBody
     public String setFunction(@PathVariable String name, @RequestParam(name = "args") String args,
             @RequestBody String body) {
+        CalculatorState calculator =
+                ((MyUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+                        .getCalculator();
+        LOG.trace("set function " + name + " to " + body + " with args " + args + " request");
         List<String> arguments = Arrays.asList(args.split(","));
         if (args.length() == 0) {
             arguments = Collections.emptyList();
         }
         calculator.addFunction(name, arguments, body);
+        updateUser(calculator);
         return "";
     }
 
     @RequestMapping(value = "function/{name}", method = DELETE, consumes = "text/plain", produces = "text/plain")
     @ResponseBody
     public ResponseEntity<String> deleteFunction(@PathVariable String name) {
+        CalculatorState calculator =
+                ((MyUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+                        .getCalculator();
+        LOG.trace("delete function " + name + " request");
         if (calculator.deleteFunction(name)) {
             return new ResponseEntity<>("", HttpStatus.OK);
         }
@@ -96,14 +136,29 @@ public class MyREST {
 
     @RequestMapping(value = "/eval", method = POST, consumes = "text/plain", produces = "text/plain")
     @ResponseBody
-    public ResponseEntity<String> evaluate(@RequestBody byte[] bodyBytes) {
-        String body = new String(bodyBytes, Charset.forName("ascii"));
+    public ResponseEntity<String> evaluate(@RequestBody String body) {
+        CalculatorState calculator =
+                ((MyUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+                        .getCalculator();
+        LOG.trace("evaluate expression '" + body + "' request");
+
+        //        SecurityContextHolder.getContext().getAuthentication().get
+
         try {
             return new ResponseEntity<String>(Double.toString(calculator.calculate(body)),
                     HttpStatus.OK);
         } catch (ParsingException e) {
-            return new ResponseEntity<String>("",
-                    HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<String>("", HttpStatus.BAD_REQUEST);
         }
+    }
+
+    CalculatorState getCalculator(String name) {
+        return userDao.loadUser(SecurityContextHolder.getContext().getAuthentication().getName())
+                .getCalculator();
+    }
+
+    void updateUser(CalculatorState calculatorWithMethods) {
+        userDao.updateCalculator(SecurityContextHolder.getContext().getAuthentication().getName(),
+                calculatorWithMethods);
     }
 }
